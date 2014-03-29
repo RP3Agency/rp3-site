1587933b4325512c081349d964221b5f4d30ea6d
Tracy Rotton <trotton@rp3agency.com>
2014-03-28 22:53:36 -0400

    latest concept of the rp3.1 home page

--- prototype/src/js/main.js
+++ prototype/src/js/main.js
@@ -1,21 +1,87 @@
-;(function($) {
+/* jshint browser:true, jquery:true */
+
+var rp3 = (function($) {
 
 	'use strict';
 
-	// $('#slide-1').waypoint( function( direction ) {
-	// 	if ( direction === 'down' ) {
-	// 		$(this).css( 'position', 'absolute' ).css( 'top', 0 );
-	// 	} else {
-	// 		$(this).css( 'position', 'relative' );
-	// 	}
-	// });
+	var 
 
-	// $(window).scroll(function () {
-	//     if ( $(window).scrollTop() > 779 ) {
-	//         $('#slide-1').css( 'top', $(window).scrollTop() ).css( 'position', 'relative' );
-	//         alert( 'foobar' );
-	//     }
-	// });
+	/**
+	 * setHomePageFrames
+	 */
+	setHomePageFrames = function() {
+		var clientHeight = document.documentElement.clientHeight,
+			$splash = $('#splash'),
+			$interstitial = $('#interstitial'),
+			$caseStudies = $('#case-studies');
 
-	// $('#video-wrapper').parallax( '50%', 0.1 );
+		if ( isiPhone() ) {
+			clientHeight = clientHeight + 69;
+		}
+
+		$splash.height(clientHeight);
+		$interstitial.height( clientHeight ).css( 'margin-top', clientHeight + 'px' );
+
+		$caseStudies.find('article').height( clientHeight );
+		$caseStudies.find('one').css( 'margin-top', ( clientHeight * 2 ) + 'px' );
+		$caseStudies.find('two').css( 'margin-top', ( clientHeight * 3 ) + 'px' );
+	},
+
+	homePageVideo = function() {
+		var clientWidth = document.documentElement.clientWidth,
+			$splash = $('#splash'),
+			$splashSlideshow = $splash.find('#splash-slideshow'),
+			$video;
+
+		$video = $('<div>').attr('id', 'video-wrapper').addClass('video-wrapper')
+			.append( $('<video autoplay loop muted>').append( $('<source>').attr('src', 'video/background.m4v') ) );
+
+		if ( clientWidth >= 640 ) {
+			$splashSlideshow.remove();
+			$splash.append( $video );
+		}
+	},
+
+	homePageAccordion = function() {
+		$('#accordion').accordion({
+			header: 'h1',
+			collapsible: true,
+			active: false,
+			heightStyle: 'content'
+		});
+	},
+
+	/**
+	 * Determine if we're on an iPhone/iPod Touch
+	 * browser sniffing. yuck.
+	 * HT http://www.ethanhackett.com/?blog=window-height-100-on-mobile-safari-coding-solution
+	 */
+	isiPhone = function() {
+		var nAgt = navigator.userAgent,
+			nPlt = navigator.platform;
+
+		if ( nPlt.indexOf( 'iPhone' ) !== -1 || nPlt.indexOf( 'iPod' ) !== -1 ) {
+			if ( ( nAgt.indexOf( 'Safari' ) ) !== -1 ) {
+				return true;
+			}
+		}
+
+		return false;
+	},
+
+	init = function() {
+		setHomePageFrames();
+		homePageVideo();
+		homePageAccordion();
+	};
+
+	return {
+		init:init
+	};
+
 }(jQuery));
+
+(function() {
+	'use strict';
+	rp3.init();
+}());
