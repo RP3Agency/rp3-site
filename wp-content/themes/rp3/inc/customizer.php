37ed695a13e0d057c977d6acfd2e5c3ccc3f43ca
Tracy Rotton <trotton@rp3agency.com>
2014-04-14 17:47:44 -0400

    moved customizer js to admin-js

--- wp-content/themes/rp3/inc/customizer.php
+++ wp-content/themes/rp3/inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'rp3_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function rp3_customize_preview_js() {
-	wp_enqueue_script( 'rp3_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'rp3_customizer', get_template_directory_uri() . '/admin-js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'rp3_customize_preview_js' );
