a2894903f1e8185160aba02d8f51fd5ffb8f181b
Tracy Rotton <trotton@rp3agency.com>
2014-04-14 17:46:51 -0400

    * commented out "site branding" content * changed menu toggle to a <button>

--- wp-content/themes/rp3/header.php
+++ wp-content/themes/rp3/header.php
@@ -22,13 +22,13 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+		<?php /* <div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+		</div> */ ?>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'rp3' ); ?></h1>
+			<button class="menu-toggle"><?php _e( 'Menu', 'rp3' ); ?></button>
 			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'rp3' ); ?></a>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
