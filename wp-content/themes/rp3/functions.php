0e869ed6f1030e3f742e3190cf3a935e96d70dac
Tracy Rotton <trotton@rp3agency.com>
2014-04-14 17:44:49 -0400

    * enabled featured image support * concatenated included JS * requiring custom image size and custom filter include files

--- wp-content/themes/rp3/functions.php
+++ wp-content/themes/rp3/functions.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * RP3 functions and definitions
@@ -38,7 +39,7 @@ function rp3_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
@@ -86,9 +87,7 @@ add_action( 'widgets_init', 'rp3_widgets_init' );
 function rp3_scripts() {
 	wp_enqueue_style( 'rp3-style', get_stylesheet_directory_uri() . '/css/rp3.min.css' );
 
-	wp_enqueue_script( 'rp3-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'rp3-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'rp3-javascript', get_template_directory_uri() . '/js/rp3.min.js', array( 'jquery' ), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -120,3 +119,13 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Custom filters for this theme.
+ */
+require get_template_directory() . '/inc/filters.php';
+
+/**
+ * Custom image sizes for this theme.
+ */
+require get_template_directory() . '/inc/image-sizes.php';
