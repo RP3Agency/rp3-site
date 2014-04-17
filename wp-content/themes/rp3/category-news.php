80aa1f29effe9fca9e1d601c0ea46778555a193c
Tracy Rotton <trotton@rp3agency.com>
2014-04-17 12:09:41 -0400

    moved page-title to above the content containers; using specific sidebar template for the news section

--- wp-content/themes/rp3/category-news.php
+++ wp-content/themes/rp3/category-news.php
@@ -9,73 +9,73 @@
 
 get_header(); ?>
 
+	<header class="page-header">
+		<h1 class="page-title">
+			<?php
+				if ( is_category() ) :
+					single_cat_title();
+
+				elseif ( is_tag() ) :
+					single_tag_title();
+
+				elseif ( is_author() ) :
+					printf( __( 'Author: %s', 'rp3' ), '<span class="vcard">' . get_the_author() . '</span>' );
+
+				elseif ( is_day() ) :
+					printf( __( 'Day: %s', 'rp3' ), '<span>' . get_the_date() . '</span>' );
+
+				elseif ( is_month() ) :
+					printf( __( 'Month: %s', 'rp3' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'rp3' ) ) . '</span>' );
+
+				elseif ( is_year() ) :
+					printf( __( 'Year: %s', 'rp3' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'rp3' ) ) . '</span>' );
+
+				elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+					_e( 'Asides', 'rp3' );
+
+				elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+					_e( 'Galleries', 'rp3');
+
+				elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+					_e( 'Images', 'rp3');
+
+				elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+					_e( 'Videos', 'rp3' );
+
+				elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+					_e( 'Quotes', 'rp3' );
+
+				elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+					_e( 'Links', 'rp3' );
+
+				elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+					_e( 'Statuses', 'rp3' );
+
+				elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+					_e( 'Audios', 'rp3' );
+
+				elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+					_e( 'Chats', 'rp3' );
+
+				else :
+					_e( 'Archives', 'rp3' );
+
+				endif;
+			?>
+		</h1>
+		<?php
+			// Show an optional term description.
+			$term_description = term_description();
+			if ( ! empty( $term_description ) ) :
+				printf( '<div class="taxonomy-description">%s</div>', $term_description );
+			endif;
+		?>
+	</header><!-- .page-header -->
+
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'rp3' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'rp3' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'rp3' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'rp3' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'rp3' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'rp3' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'rp3' );
-
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'rp3');
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'rp3');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'rp3' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'rp3' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'rp3' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'rp3' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'rp3' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'rp3' );
-
-						else :
-							_e( 'Archives', 'rp3' );
-
-						endif;
-					?>
-				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -95,5 +95,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<?php get_sidebar( 'news' ); ?>
 <?php get_footer(); ?>
