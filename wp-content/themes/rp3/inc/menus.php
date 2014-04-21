984d250f6a20c8d0846292bbdfb26ebc79c1a3b0
Tracy Rotton <trotton@rp3agency.com>
2014-04-21 17:10:45 -0400

    tweaking how the menus are named [#424]

--- wp-content/themes/rp3/inc/menus.php
+++ wp-content/themes/rp3/inc/menus.php
@@ -4,9 +4,9 @@
 function rp3_register_nav_menus() {
 
 	register_nav_menus( array(
-		'primary-left'	=> __( 'Primary Menu (Left)', 'rp3' ),
-		'primary-right'	=> __( 'Primary Menu (Right)', 'rp3' ),
-		'header_aux'	=> __( 'Header Auxiliary', 'rp3' )
+		'primary-left'		=> __( 'Primary Menu (Left)', 'rp3' ),
+		'primary-right'		=> __( 'Primary Menu (Right)', 'rp3' ),
+		'header-auxiliary'	=> __( 'Header Auxiliary', 'rp3' )
 	) );
 }
 add_action( 'init', 'rp3_register_nav_menus' );
