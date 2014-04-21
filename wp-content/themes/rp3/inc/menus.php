94c627d8d6f76e71e559f952c3a894ef594d2781
Tracy Rotton <trotton@rp3agency.com>
2014-04-21 09:11:32 -0400

    updating for left/right primary menus [#424]

--- wp-content/themes/rp3/inc/menus.php
+++ wp-content/themes/rp3/inc/menus.php
@@ -4,8 +4,9 @@
 function rp3_register_nav_menus() {
 
 	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'rp3' ),
-		'header_aux' => __( 'Header Auxiliary', 'rp3' )
+		'primary-left'	=> __( 'Primary Menu (Left)', 'rp3' ),
+		'primary-right'	=> __( 'Primary Menu (Right)', 'rp3' ),
+		'header_aux'	=> __( 'Header Auxiliary', 'rp3' )
 	) );
 }
 add_action( 'init', 'rp3_register_nav_menus' );
