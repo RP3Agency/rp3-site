450a422a918ba6123fb556187bc58dffcfe8e696
Tracy Rotton <trotton@rp3agency.com>
2014-04-25 22:42:16 -0400

    footer progress! [#425]

--- wp-content/themes/rp3/footer.php
+++ wp-content/themes/rp3/footer.php
@@ -16,22 +16,26 @@
 
 				<div class="site-info">
 
-					<h1>RP3 Agency</h1>
+					<h1 class="logo">RP3 Agency</h1>
 
 					<ul class="social">
 						<li class="facebook"><a href="https://www.facebook.com/rp3agency">Facebook</a></li>
 						<li class="twitter"><a href="https://twitter.com/rp3agency">Twitter</a></li>
 						<li class="linkedin"><a href="https://www.linkedin.com/company/rp3-agency">LinkedIn</a></li>
+					</ul>
+					<ul class="social">
 						<li class="github"><a href="https://github.com/RP3Agency">Github</a></li>
 						<li class="instagram"><a href="http://instagram.com/rp3agency">Instagram</a></li>
 						<li class="pinterest"><a href="http://www.pinterest.com/rp3agency/">Pinterest</a></li>
 					</ul>
 
+					<a href="mailto:info@rp3agency.com" class="email">info@rp3agency.com</a>
+
 				</div>
 
 			</div>
 
-			<div class="site-info">
+			<a href="https://www.google.com/maps/place/7316+Wisconsin+Ave/@38.982666,-77.094007,18z/data=!3m1!4b1!4m2!3m1!1s0x89b7c97b02dc5265:0x1dfbc833b8797b9b" class="site-info">
 
 				<div class="address">
 					7316 Wisconsin Avenue<br>
@@ -39,7 +43,7 @@
 					Bethesda, Maryland 20814
 				</div>
 
-			</div>
+			</a>
 
 		</div>
 
