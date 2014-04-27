3b2e17779e8b1153e3359eb578b17b4d03879f90
Tracy Rotton <trotton@rp3agency.com>
2014-04-27 16:49:30 -0400

    finishing up the footer [#425]

--- wp-content/themes/rp3/footer.php
+++ wp-content/themes/rp3/footer.php
@@ -12,73 +12,81 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 
-			<div class="top">
+		<div class="top">
 
-				<div class="site-info">
+			<div class="site-info">
 
-					<h1 class="logo">RP3 Agency</h1>
+				<h1 class="logo">RP3 Agency</h1>
 
-					<section class="left">
-						<h2>Heading 2</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>
-					</section>
-					<!-- // .footer-blurb -->
+				<section class="left">
+					<h2>Heading 2</h2>
 
-					<div class="center">
-						<ul class="social">
-							<li class="facebook"><a href="https://www.facebook.com/rp3agency">Facebook</a></li>
-							<li class="twitter"><a href="https://twitter.com/rp3agency">Twitter</a></li>
-							<li class="linkedin"><a href="https://www.linkedin.com/company/rp3-agency">LinkedIn</a></li>
-						</ul>
-						<ul class="social">
-							<li class="github"><a href="https://github.com/RP3Agency">Github</a></li>
-							<li class="instagram"><a href="http://instagram.com/rp3agency">Instagram</a></li>
-							<li class="pinterest"><a href="http://www.pinterest.com/rp3agency/">Pinterest</a></li>
-						</ul>
-					</div>
-					<!-- // .social-media -->
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>
 
-					<section class="right">
-						<h2>Contact</h2>
+					<p><a href="#!">Download our fact sheet.</a></p>
+				</section>
+				<!-- // .footer-blurb -->
 
-						<span class="address">
-							7316 Wisconsin Avenue<br>
-							Suite 450<br>
-							Bethesda, Maryland 20814<br>
-							<a href="https://www.google.com/maps/place/7316+Wisconsin+Ave/@38.982666,-77.094007,18z/data=!3m1!4b1!4m2!3m1!1s0x89b7c97b02dc5265:0x1dfbc833b8797b9b">View on Google Maps</a>
-						</span>
+				<div class="right">
+					<ul class="social">
+						<li class="facebook"><a href="https://www.facebook.com/rp3agency">Facebook</a></li>
+						<li class="twitter"><a href="https://twitter.com/rp3agency">Twitter</a></li>
+						<li class="linkedin"><a href="https://www.linkedin.com/company/rp3-agency">LinkedIn</a></li>
+					</ul>
+					<ul class="social">
+						<li class="github"><a href="https://github.com/RP3Agency">Github</a></li>
+						<li class="instagram"><a href="http://instagram.com/rp3agency">Instagram</a></li>
+						<li class="pinterest"><a href="http://www.pinterest.com/rp3agency/">Pinterest</a></li>
+					</ul>
 
-						<a href="mailto:info@rp3agency.com" class="email">info@rp3agency.com</a>
+					<a href="mailto:info@rp3agency.com" class="email">info@rp3agency.com</a>
 
-						<div class="phone">
-							t. 301.718.0333<br>
-							f. 301.718.9333
-						</div>
-
-						<h2><a href="<?php echo esc_url( home_url( '/careers/' ) ); ?>">Careers</a></h2>
-					</section>
+					<form>
+						<label>Sign up for our newsletter:
+							<input type="email" name="newsletter">
+						</label>
+					</form>
 
 				</div>
+				<!-- // .social-media -->
 
 			</div>
+			<!-- // .site-info -->
 
-			<a href="https://www.google.com/maps/place/7316+Wisconsin+Ave/@38.982666,-77.094007,18z/data=!3m1!4b1!4m2!3m1!1s0x89b7c97b02dc5265:0x1dfbc833b8797b9b" class="site-info">
+		</div>
+		<!-- // .top -->
+
+		<div class="bottom">
+
+			<div class="site-info">
+
+				<div class="phone">
+					t. 301.718.0333<br>
+					f. 301.718.9333<br>
+					<span class="copyright">© <?php echo date('Y'); ?> RP3 Agency</span>
+				</div>
+				<!-- // .phone -->
 
 				<div class="address">
+					<a href="https://www.google.com/maps/place/7316+Wisconsin+Ave/@38.982666,-77.094007,18z/data=!3m1!4b1!4m2!3m1!1s0x89b7c97b02dc5265:0x1dfbc833b8797b9b">
 					7316 Wisconsin Avenue<br>
 					Suite 450<br>
 					Bethesda, Maryland 20814
+					</a>
 				</div>
+				<!-- // .address -->
 
-			</a>
+			</div>
+			<!-- // .site-info -->
 
 		</div>
+		<!-- // .bottom -->
 
 	</footer>
-	<!-- #colophon -->
+	<!-- // #colophon -->
 
 </div>
-<!-- #page -->
+<!-- // #page -->
 
 <?php wp_footer(); ?>
 
