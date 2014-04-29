212cee227026e2458298c4a1d81790e26ce4e222
Tracy Rotton <trotton@rp3agency.com>
2014-04-29 08:07:40 -0400

    beginning of new custom plugin development [#443]

---
+++ wp-content/plugins/rp3/rp3.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: RP3 Custom Functionality
+Description: Custom post types and taxonomies and other custom functionality used by RP3 that should not be tied to the theme
+Version: 3.1
+Author: Tracy Rotton
+Author URI: http://rp3agency.com
+*/
+
