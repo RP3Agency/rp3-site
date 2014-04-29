ca977e26aa07959cd39224e4539cbd7b2d752f5b
Tracy Rotton <trotton@rp3agency.com>
2014-04-29 07:46:17 -0400

    creating widget areas for the blog archive page [#421]

--- wp-content/themes/rp3/sidebar-blog-archive.php
+++ wp-content/themes/rp3/sidebar-blog-archive.php
@@ -5,13 +5,18 @@
  * @package RP3
  */
 ?>
-<section id="secondary" class="widget-area blog-archive-widget-area" role="complementary">
 
-	<?php for ( $i = 1; $i < 4; $i++ ) : ?>
+<section id="#secondary" class="blog-archive-widget-area" role="complementary">
+
+<?php for ( $i = 1; $i < 4; $i++ ) : ?>
+
+	<div class="widget-area">
 
 		<?php dynamic_sidebar( 'blog-archive-' . $i ); ?>
 
-	<?php endfor; ?>
+	</div>
+
+<?php endfor; ?>
 
 </section>
 <!-- #secondary -->
