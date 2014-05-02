0c8cbb90fdd1d10ccb041b38f610a1bc16a83fc9
Tracy Rotton <trotton@rp3agency.com>
2014-05-02 16:39:44 -0400

    added a wrapper around the header and added 1em of bottom margin to it. [#431]

--- wp-content/themes/rp3/header.php
+++ wp-content/themes/rp3/header.php
@@ -21,61 +21,66 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	<header id="masthead" class="site-header" role="banner">
+	<div class="header-container">
 
-		<!-- <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'rp3' ); ?></a> -->
+		<header id="masthead" class="site-header" role="banner">
 
-		<div class="header-wrapper">
+			<!-- <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'rp3' ); ?></a> -->
 
-			<!-- Site Branding -->
-			<div class="site-branding">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<div class="header-wrapper">
 
-				<a href="#!" id="menu-open" class="menu-open">Menu</a>
+				<!-- Site Branding -->
+				<div class="site-branding">
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 
-				<!-- Header Aux -->
+					<a href="#!" id="menu-open" class="menu-open">Menu</a>
 
-				<div class="header-aux">
-					<a href="#!" class="search-toggle">Search</a>
-					<?php wp_nav_menu( array(
-						'theme_location'	=> 'header-auxiliary',
-						'menu_class'		=> 'menu social'
-					) ); ?>
+					<!-- Header Aux -->
+
+					<div class="header-aux">
+						<a href="#!" class="search-toggle">Search</a>
+						<?php wp_nav_menu( array(
+							'theme_location'	=> 'header-auxiliary',
+							'menu_class'		=> 'menu social'
+						) ); ?>
+					</div>
+					<!-- // .header-aux -->
+
 				</div>
-				<!-- // .header-aux -->
+				<!-- // .site-branding -->
 
 			</div>
-			<!-- // .site-branding -->
+			<!-- // .header-wrapper -->
 
-		</div>
-		<!-- // .header-wrapper -->
+			<?php get_template_part( 'searchform', 'header-aux' ); ?>
 
-		<?php get_template_part( 'searchform', 'header-aux' ); ?>
+		</header>
+		<!-- // #masthead -->
 
-	</header>
-	<!-- // #masthead -->
+		<!-- Primary Navigation -->
 
-	<!-- Primary Navigation -->
+		<nav id="site-navigation" class="main-navigation" role="navigation">
 
-	<nav id="site-navigation" class="main-navigation" role="navigation">
+			<div id="main-nav" class="main-nav">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo" rel="home"><?php bloginfo( 'name' ); ?></a>
+				<?php wp_nav_menu( array(
+					'theme_location'	=> 'primary-left',
+					'menu_class'		=> 'primary-menu-left'
+				) ); ?>
+				<?php wp_nav_menu( array(
+					'theme_location'	=> 'primary-right',
+					'menu_class'		=> 'primary-menu-right'
+				) ); ?>
+			</div>
+			<!-- // #main-nav -->
 
-		<div id="main-nav" class="main-nav">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<?php wp_nav_menu( array(
-				'theme_location'	=> 'primary-left',
-				'menu_class'		=> 'primary-menu-left'
-			) ); ?>
-			<?php wp_nav_menu( array(
-				'theme_location'	=> 'primary-right',
-				'menu_class'		=> 'primary-menu-right'
-			) ); ?>
-		</div>
-		<!-- // #main-nav -->
+			<?php get_template_part( 'searchform', 'nav' ); ?>
 
-		<?php get_template_part( 'searchform', 'nav' ); ?>
+		</nav>
+		<!-- // #site-navigation -->
 
-	</nav>
-	<!-- // #site-navigation -->
+	</div>
+	<!-- // header-container -->
 
 	<div id="content" class="site-content">
