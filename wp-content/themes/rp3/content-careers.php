f2532d17d948ef6ade79d7ef840bb47efdefec40
Tracy Rotton <trotton@rp3agency.com>
2014-05-20 10:50:06 -0400

    restoring the footer code [#453]

--- wp-content/themes/rp3/content-careers.php
+++ wp-content/themes/rp3/content-careers.php
@@ -25,6 +25,43 @@ $careers = new WP_Query( array(
 				<?php the_excerpt(); ?>
 			</div><!-- .entry-summary -->
 
+			<footer class="entry-footer">
+				<?php if ( ! is_archive() ) : ?>
+	
+				<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+					<?php
+						/* translators: used between list items, there is a space after the comma */
+						$categories_list = get_the_category_list( __( ', ', 'rp3' ) );
+						if ( $categories_list && rp3_categorized_blog() ) :
+					?>
+					<span class="cat-links">
+						<?php printf( __( 'Posted in %1$s', 'rp3' ), $categories_list ); ?>
+					</span>
+					<?php endif; // End if categories ?>
+
+					<?php
+						/* translators: used between list items, there is a space after the comma */
+						$tags_list = get_the_tag_list( '', __( ', ', 'rp3' ) );
+						if ( $tags_list ) :
+					?>
+					<span class="tags-links">
+						<?php printf( __( 'Tagged %1$s', 'rp3' ), $tags_list ); ?>
+					</span>
+					<?php endif; // End if $tags_list ?>
+				<?php endif; // End if 'post' == get_post_type() ?>
+
+				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) && ( ! has_category('news') ) ) : ?>
+				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'rp3' ), __( '1 Comment', 'rp3' ), __( '% Comments', 'rp3' ) ); ?></span>
+				<?php endif; ?>
+
+				<?php edit_post_link( __( 'Edit', 'rp3' ), '<span class="edit-link">', '</span>' ); ?>
+
+				<?php else: ?>
+				<p><i>Continue reading &rarr;</i></p>
+				<?php endif; // ! is_archive() ?>
+
+			</footer><!-- .entry-footer -->
+
 		</div>
 		<!-- // .entry-wrapper -->
 
