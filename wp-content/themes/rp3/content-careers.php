78a7f56be16e157647e661102afed1166b378656
Tracy Rotton <trotton@rp3agency.com>
2014-06-03 11:25:29 -0400

    fixed all my careers archive page problems with a simple filter. huzzah!

--- wp-content/themes/rp3/content-careers.php
+++ wp-content/themes/rp3/content-careers.php
@@ -4,8 +4,7 @@
  */
 
 $careers = new WP_Query( array(
-	'category_name'			=> 'careers',
-	'posts_per_page'		=> 1
+	'category_name'			=> 'careers'
 ));
 ?>
 
@@ -22,44 +21,13 @@ $careers = new WP_Query( array(
 			</header><!-- .entry-header -->
 
 			<div class="entry-summary">
+				<?php // We don't want sharing links here, exactly. ?>
+				<?php remove_filter( 'the_excerpt', 'sharing_display', 19 ); ?>
 				<?php the_excerpt(); ?>
 			</div><!-- .entry-summary -->
 
 			<footer class="entry-footer">
-				<?php if ( ! is_archive() ) : ?>
-	
-				<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-					<?php
-						/* translators: used between list items, there is a space after the comma */
-						$categories_list = get_the_category_list( __( ', ', 'rp3' ) );
-						if ( $categories_list && rp3_categorized_blog() ) :
-					?>
-					<span class="cat-links">
-						<?php printf( __( 'Posted in %1$s', 'rp3' ), $categories_list ); ?>
-					</span>
-					<?php endif; // End if categories ?>
-
-					<?php
-						/* translators: used between list items, there is a space after the comma */
-						$tags_list = get_the_tag_list( '', __( ', ', 'rp3' ) );
-						if ( $tags_list ) :
-					?>
-					<span class="tags-links">
-						<?php printf( __( 'Tagged %1$s', 'rp3' ), $tags_list ); ?>
-					</span>
-					<?php endif; // End if $tags_list ?>
-				<?php endif; // End if 'post' == get_post_type() ?>
-
-				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) && ( ! has_category('news') ) ) : ?>
-				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'rp3' ), __( '1 Comment', 'rp3' ), __( '% Comments', 'rp3' ) ); ?></span>
-				<?php endif; ?>
-
-				<?php edit_post_link( __( 'Edit', 'rp3' ), '<span class="edit-link">', '</span>' ); ?>
-
-				<?php else: ?>
-				<p><i>Continue reading &rarr;</i></p>
-				<?php endif; // ! is_archive() ?>
-
+				<p><i>Learn more &rarr;</i></p>
 			</footer><!-- .entry-footer -->
 
 		</div>
