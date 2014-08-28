2a5c728764c73d94a25644b8266e965e395b936e
Tracy Rotton <trotton@rp3agency.com>
2014-08-27 22:37:36 -0400

    prototyping by adding greeked content straight into the template, to be replaced by live data on a not-too-distant-future date. [#479]

--- wp-content/themes/rp3/content-page-news-views.php
+++ wp-content/themes/rp3/content-page-news-views.php
@@ -15,9 +15,185 @@
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 
-	<section class="articles">
+	<section class="row articles">
 
-		
+		<div class="blog">
+
+			<div class="row">
+
+				<article>
+
+					<a href="#!">
+
+						<header>
+							<div class="entry-category">Blog</div>
+							<!-- <div class="entry-gravatar"><img src="http://placehold.it/50x50"></div> -->
+							<h1 class="entry-title">Lorem ipsum.</h1>
+						</header>
+
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam 
+						rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+						Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni 
+						dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor 
+						sit amet, consectetur, adipisci velit, sed quia non........</p>
+
+						<p>Continue reading Lorem Ipsum.</p>
+
+						<footer class="entry-meta">
+							<div>Written by <strong>Jack Jefferson</strong> on October 21, 2014 in <strong>Design</strong>, <strong>News</strong>.</div>
+						</footer>
+
+					</a>
+
+				</article>
+
+			</div>
+			<!-- // .row -->
+
+			<div class="row">
+
+				<article>
+
+					<a href="#!">
+
+						<header>
+							<div class="entry-category">Blog</div>
+							<!-- <div class="entry-gravatar"><img src="http://placehold.it/50x50"></div> -->
+							<h1 class="entry-title">Lorem ipsum.</h1>
+						</header>
+
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam 
+						rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+						Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni 
+						dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor 
+						sit amet, consectetur, adipisci velit, sed quia non........</p>
+
+						<p>Continue reading Lorem Ipsum.</p>
+
+						<footer class="entry-meta">
+							<div>Written by <strong>Jack Jefferson</strong> on October 21, 2014 in <strong>Design</strong>, <strong>News</strong>.</div>
+						</footer>
+
+					</a>
+
+				</article>
+
+			</div>
+			<!-- // .row -->
+
+			<div class="row">
+
+				<article>
+
+					<a href="#!">
+
+						<header>
+							<div class="entry-category">Blog</div>
+							<!-- <div class="entry-gravatar"><img src="http://placehold.it/50x50"></div> -->
+							<h1 class="entry-title">Lorem ipsum.</h1>
+						</header>
+
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam 
+						rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+						Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni 
+						dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor 
+						sit amet, consectetur, adipisci velit, sed quia non........</p>
+
+						<p>Continue reading Lorem Ipsum.</p>
+
+						<footer class="entry-meta">
+							<div>Written by <strong>Jack Jefferson</strong> on October 21, 2014 in <strong>Design</strong>, <strong>News</strong>.</div>
+						</footer>
+
+					</a>
+
+				</article>
+
+				<article>
+
+					<a href="#!">
+
+						<header>
+							<div class="entry-category">Blog</div>
+							<!-- <div class="entry-gravatar"><img src="http://placehold.it/50x50"></div> -->
+							<h1 class="entry-title">Lorem ipsum.</h1>
+						</header>
+
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam 
+						rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+						Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni 
+						dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor 
+						sit amet, consectetur, adipisci velit, sed quia non........</p>
+
+						<p>Continue reading Lorem Ipsum.</p>
+
+						<footer class="entry-meta">
+							<div>Written by <strong>Jack Jefferson</strong> on October 21, 2014 in <strong>Design</strong>, <strong>News</strong>.</div>
+						</footer>
+
+					</a>
+
+				</article>
+
+			</div>
+			<!-- // .row -->
+
+			<div class="row">
+
+				<article>
+
+					<a href="#!">
+
+						<header>
+							<div class="entry-category">Blog</div>
+							<!-- <div class="entry-gravatar"><img src="http://placehold.it/50x50"></div> -->
+							<h1 class="entry-title">Lorem ipsum.</h1>
+						</header>
+
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam 
+						rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+						Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni 
+						dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor 
+						sit amet, consectetur, adipisci velit, sed quia non........</p>
+
+						<p>Continue reading Lorem Ipsum.</p>
+
+						<footer class="entry-meta">
+							<div>Written by <strong>Jack Jefferson</strong> on October 21, 2014 in <strong>Design</strong>, <strong>News</strong>.</div>
+						</footer>
+
+					</a>
+
+				</article>
+
+			</div>
+			<!-- // .row -->
+
+		</div>
+		<!-- // .blog -->
+
+		<div class="news twitter">
+
+			<article class="tweet">
+
+				<header>
+					<div class="category">Twitter</div>
+				</header>
+
+				<p>What happens if you actually 'like' everything in your News Feed for 48 hrs? <a href="http://ow.ly/Acy7d">http://ow.ly/Acy7d</a> via <a href="https://twitter.com/wired">@WIRED</a></p>
+
+				<footer class="entry-meta">
+					<div class="account"><a href="https://twitter.com/rp3agency">@rp3agency</a></div>
+				</footer>
+
+			</article>
+			<!-- // .tweet -->
+
+
+		</div>
+		<!-- // .news -->
+
+
 
 
 	</section>
