ec5d5000684121029cc2d8cdb53d2d9372cac7cf
Tracy Rotton <trotton@rp3agency.com>
2014-09-12 15:01:48 -0400

    ditched home panels; created work [#489]

--- wp-content/plugins/rp3/inc/custom-post-types.php
+++ wp-content/plugins/rp3/inc/custom-post-types.php
@@ -47,32 +47,36 @@ function rp3_cpt_capabilities() {
 
 
 /**
- * Home Panels
- * 3 featured panels to display on the home page.
- * Will eventually link to case studies, etc.
+ * Work
+ * Work highlights and case studies.
  */
-function rp3_cpt_home_panels() {
+function rp3_cpt_work() {
 
 	// Custom Post Type
 	$args = array(
 		'labels'				=> array(
-			'name'					=> 'Home Panels',
-			'singular_name'			=> 'Panel'
+			'name'					=> 'Work',
+			'singular_name'			=> 'Work',
+			'add_new_item'			=> 'Add New Work',
+			'edit_item'				=> 'Edit Work',
+			'new_item'				=> 'New Work',
+			'view_item'				=> 'View Work',
+			'search_items'			=> 'Search Work'
 		),
-		'description'			=> 'Panels that appear on the home page',
+		'description'			=> 'Work and case studies.',
 		'public'				=> true,
 		'exclude_from_search'	=> true,
-		'publicly_queryable'	=> false,
 		'show_ui'				=> true,
 		'show_in_nav_menus'		=> false,
 		'show_in_menu'			=> true,
 		'show_in_admin_bar'		=> false,
 		'menu_position'			=> 20,
 		'hierarchical'			=> true,
-		'supports'				=> array( 'title' )
+		'menu_icon'				=> 'dashicons-portfolio',
+		'supports'				=> array( 'title', 'editor', 'thumbnail' )
 	);
 
-	register_post_type( 'rp3_home_panels', $args );
+	register_post_type( 'rp3_cpt_work', $args );
 }
 
 
@@ -85,6 +89,6 @@ function rp3_cpt_home_panels() {
  */
 function rp3_cpt() {
 	rp3_cpt_capabilities();
-	rp3_cpt_home_panels();
+	rp3_cpt_work();
 }
 add_action( 'init', 'rp3_cpt' );
