24a87f129a43f72d4b40b809e3eaa91f0baca257
Tracy Rotton <trotton@rp3agency.com>
2014-09-12 15:00:43 -0400

    breaking out components into individual template parts [#489]

---
+++ wp-content/themes/rp3/components/home-page-hero.php
@@ -0,0 +1,50 @@
+<?php $posts = get_field( 'home_page_hero' ); ?>
+
+<?php if ( $posts ) : ?>
+
+	<?php foreach ( $posts as $post ) : ?>
+
+	<?php setup_postdata( $post ); ?>
+
+		<section id="home-hero" class="home-hero">
+
+			<a href="<?php echo esc_url( get_the_permalink() ); ?>">
+
+				<?php if ( get_the_post_thumbnail( get_the_ID() ) != '' ) : ?>
+
+				<?php
+				$thumbnail_small		= wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'home-page-hero-small' );
+				$thumbnail_small_2x		= wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'home-page-hero-small-2x' );
+				$thumbnail_medium		= wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'home-page-hero-medium' );
+				$thumbnail_medium_2x	= wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'home-page-hero-medium-2x' );
+				$thumbnail_large		= wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'home-page-hero-large' );
+				$thumbnail_large_2x		= wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'home-page-hero-large-2x' );
+				?>
+
+				<picture>
+					<!--[if IE 9]><video style="display: none;"><![endif]-->
+					<source srcset="<?php echo esc_url( $thumbnail_large[0] ); ?>, <?php echo esc_url( $thumbnail_large_2x[0] ); ?> 2x" media="(min-width: 37.5em)">
+					<source srcset="<?php echo esc_url( $thumbnail_medium[0] ); ?>, <?php echo esc_url( $thumbnail_medium_2x[0] ); ?> 2x" media="(min-width: 20.0625em)">
+					<source srcset="<?php echo esc_url( $thumbnail_small[0] ); ?>, <?php echo esc_url( $thumbnail_small_2x[0] ); ?> 2x">
+					<!--[if IE 9]></video><![endif]-->
+					<img srcset="<?php echo esc_url( $thumbnail_small[0] ); ?>, <?php echo esc_url( $thumbnail_small_2x[0] ); ?> 2x" alt="<?php esc_attr( get_the_title() ); ?>">
+				</picture>
+
+				<?php endif; ?>
+
+				<div class="wrapper">
+
+					<div class="headline">
+						<?php the_field( 'headline' ); ?>
+					</div>
+
+				</div>
+				<!-- // .wrapper -->
+
+			</a>
+
+		</section>
+
+	<?php endforeach; wp_reset_postdata(); ?>
+
+<?php endif; ?>
