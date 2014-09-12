129ba65e26720452f063d736c16d7e6fd769bd25
Tracy Rotton <trotton@rp3agency.com>
2014-09-12 15:01:06 -0400

    home page responsive hero image sizes [#489]

--- wp-content/themes/rp3/inc/image-sizes.php
+++ wp-content/themes/rp3/inc/image-sizes.php
@@ -7,3 +7,12 @@
 
 // News thumbnails
 add_image_size( 'news-thumbnails', 137, 137, true );
+
+
+// Home Page Hero
+add_image_size( 'home-page-hero-small', 320, 450, true );
+add_image_size( 'home-page-hero-small-2x', 320*2, 450*2, true );
+add_image_size( 'home-page-hero-medium', 640, 450, true );
+add_image_size( 'home-page-hero-medium-2x', 640*2, 450*2, true );
+add_image_size( 'home-page-hero-large', 1600, 608, true );
+add_image_size( 'home-page-hero-large-2x', 1600*2, 608*2, true );
