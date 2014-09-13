eb3da47c38dec4b691e053d138ed3ab497a455fd
Tracy Rotton <trotton@rp3agency.com>
2014-09-12 23:07:25 -0400

    doing a little experimentation with the iphone accelerometer.

--- src/js/rp3.js
+++ src/js/rp3.js
@@ -127,3 +127,54 @@ var rp3 = (function($) {
 	'use strict';
 	rp3.init();
 }());
+
+
+/**
+ * Accelerometer Experiment
+ */
+
+jQuery(document).ready(function($) {
+
+	var has_touch = 'ontouchstart' in document.documentElement;
+	var accX, accY, width, height, xA, yA, movement;
+	
+	if(has_touch || screen.width <= 699) {
+
+		(resize = function() {
+			
+			height = $(window).height();
+			width = $(window).width();
+		
+			$('#home-work a').width(width).height(height);
+			
+			
+		})();
+		
+		window.ondevicemotion = function(event) {
+		  	
+		    accX = Math.round(event.accelerationIncludingGravity.x*10) / 10;  
+		    accY = Math.round(event.accelerationIncludingGravity.y*10) / 10;  
+		    
+		    movement = 10;
+		    
+		    xA = -(accX / 10) * movement;
+		    yA = -(accY / 10) * movement;
+		    
+		    
+			run();
+		    
+		}  
+	}
+	
+	function run() {
+		    
+	    $('picture').css({'left' : xA+'px', 'top' : yA+'px', 'box-shadow' : ''+xA+'px '+yA+'px 10px rgba(0,0,0,0.3)'});
+	    
+	    bX = -(xA*2)-100;
+	    bY = -(yA*2)-300;
+	    
+	    $('#home-work a').css({'background-position' : bX+'px '+bY+'px'});
+	
+	}
+	
+}(jQuery));
