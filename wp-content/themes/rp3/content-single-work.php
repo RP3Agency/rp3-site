a4acee566bb3c8b7c787d0377a9640976511a6d5
Tracy Rotton <trotton@rp3agency.com>
2014-09-15 15:21:18 -0400

    adding template and styling for case studies [#493]

---
+++ wp-content/themes/rp3/content-single-work.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * @package RP3
+ */
+
+// Is this a case study?
+if ( true === get_field( 'case_study' ) ) {
+	$class = 'case-study';
+}
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( $class ); ?>>
+
+	<div class="<?php echo esc_attr( $class ); ?>__entry-content">
+
+		<header class="<?php echo esc_attr( $class ); ?>__entry-header">
+
+			<?php the_content(); ?>
+
+		</header>
+		<!-- // .<?php echo esc_attr( $class ); ?>__entry-header -->
+
+
+		<?php
+		$image = get_field( 'image_1' );
+		if ( ( is_array( $image ) && ( count( $image ) > 0 ) ) ) :
+		?>
+
+		<img src="<?php echo esc_url( $image['url'] ); ?>">
+
+		<?php endif; ?>
+
+		<section class="<?php echo esc_attr( $class ); ?>__section">
+
+			<h2 class="<?php echo esc_attr( $class ); ?>__subhead">Think:</h2>
+
+			<div class="<?php echo esc_attr( $class ); ?>__content">
+
+				<?php the_field( 'think' ); ?>
+
+			</div>
+
+		</section>
+		<!-- // .<?php echo esc_attr( $class ); ?>__section -->
+
+
+		<?php
+		$image = get_field( 'image_2' );
+		if ( ( is_array( $image ) && ( count( $image ) > 0 ) ) ) :
+		?>
+
+		<img src="<?php echo esc_url( $image['url'] ); ?>">
+
+		<?php endif; ?>
+
+
+		<section class="<?php echo esc_attr( $class ); ?>__section">
+
+			<h2 class="<?php echo esc_attr( $class ); ?>__subhead">Feel:</h2>
+
+			<div class="<?php echo esc_attr( $class ); ?>__content">
+
+				<?php the_field( 'feel' ); ?>
+
+			</div>
+
+		</section>
+		<!-- // .<?php echo esc_attr( $class ); ?>__section -->
+
+
+		<?php
+		$image = get_field( 'image_3' );
+		if ( ( is_array( $image ) && ( count( $image ) > 0 ) ) ) :
+		?>
+
+		<img src="<?php echo esc_url( $image['url'] ); ?>">
+
+		<?php endif; ?>
+
+
+		<section class="<?php echo esc_attr( $class ); ?>__section">
+
+			<h2 class="<?php echo esc_attr( $class ); ?>__subhead">Build:</h2>
+
+			<div class="<?php echo esc_attr( $class ); ?>__content">
+
+				<?php the_field( 'build' ); ?>
+
+			</div>
+
+		</section>
+		<!-- // .<?php echo esc_attr( $class ); ?>__section -->
+
+
+		<?php
+		$image = get_field( 'image_4' );
+		if ( ( is_array( $image ) && ( count( $image ) > 0 ) ) ) :
+		?>
+
+		<img src="<?php echo esc_url( $image['url'] ); ?>">
+
+		<?php endif; ?>
+
+
+	</div>
+	<!-- .<?php echo esc_attr( $class ); ?>__entry-content -->
+
+	</footer>
+	<!-- .entry-footer -->
+
+</article>
+<!-- #post-## -->
