0da6cc88a36e5cc28cb5f0a1ee9099fc28c80592
Tracy Rotton <trotton@rp3agency.com>
2014-09-22 11:47:16 -0400

    normalized the indentation [#497]

--- src/js/rp3.js
+++ src/js/rp3.js
@@ -141,41 +141,41 @@ jQuery(document).ready(function($) {
 	if(has_touch) {
 
 		(resize = function() {
-			
+
 			height = $(window).height();
 			width = $(window).width();
-		
+
 			$('#home-work a').width(width).height(height);
-			
-			
+
+
 		})();
-		
+
 		window.ondevicemotion = function(event) {
-		  	
-		    accX = Math.round(event.accelerationIncludingGravity.x*10) / 10;  
-		    accY = Math.round(event.accelerationIncludingGravity.y*10) / 10;  
-		    
-		    movement = 10;
-		    
-		    xA = -(accX / 10) * movement;
-		    yA = -(accY / 10) * movement;
-		    
-		    
+
+			accX = Math.round(event.accelerationIncludingGravity.x*10) / 10;
+			accY = Math.round(event.accelerationIncludingGravity.y*10) / 10;
+
+			movement = 10;
+
+			xA = -(accX / 10) * movement;
+			yA = -(accY / 10) * movement;
+
+
 			run();
-		    
-		}  
+
+		}
 	}
 	
 	function run() {
-		    
-	    $('#home-work picture').css({'left' : xA+'px', 'top' : yA+'px',
+
+		$('#home-work picture').css({'left' : xA+'px', 'top' : yA+'px',
 			'box-shadow' : ''+xA+'px '+yA+'px 10px rgba(0,0,0,0.3)'});
-	    
-	    bX = -(xA*2)-100;
-	    bY = -(yA*2)-300;
-	    
-	    // $('#home-work a').css({'background-position' : bX+'px '+bY+'px'});
-	
+
+		bX = -(xA*2)-100;
+		bY = -(yA*2)-300;
+
+		// $('#home-work a').css({'background-position' : bX+'px '+bY+'px'});
+
 	}
 	
 }(jQuery));
@@ -185,148 +185,152 @@ jQuery(document).ready(function($) {
  * Google maps
  */
 function initialize() {
-	var myLatLng = new google.maps.LatLng(38.9827398,-77.0940005);
-	var mapOptions = {
-		center: myLatLng,
-		zoom: 11,
-		styles: [
-		    {
-		        "stylers": [
-		            {
-		                "saturation": -100
-		            },
-		            {
-		                "gamma": 1
-		            }
-		        ]
-		    },
-		    {
-		        "elementType": "labels.text.stroke",
-		        "stylers": [
-		            {
-		                "visibility": "off"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "poi.business",
-		        "elementType": "labels.text",
-		        "stylers": [
-		            {
-		                "visibility": "off"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "poi.business",
-		        "elementType": "labels.icon",
-		        "stylers": [
-		            {
-		                "visibility": "off"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "poi.place_of_worship",
-		        "elementType": "labels.text",
-		        "stylers": [
-		            {
-		                "visibility": "off"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "poi.place_of_worship",
-		        "elementType": "labels.icon",
-		        "stylers": [
-		            {
-		                "visibility": "off"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "road",
-		        "elementType": "geometry",
-		        "stylers": [
-		            {
-		                "visibility": "simplified"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "water",
-		        "stylers": [
-		            {
-		                "visibility": "on"
-		            },
-		            {
-		                "saturation": 50
-		            },
-		            {
-		                "gamma": 0
-		            },
-		            {
-		                "hue": "#50a5d1"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "administrative.neighborhood",
-		        "elementType": "labels.text.fill",
-		        "stylers": [
-		            {
-		                "color": "#333333"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "road.local",
-		        "elementType": "labels.text",
-		        "stylers": [
-		            {
-		                "weight": 0.5
-		            },
-		            {
-		                "color": "#333333"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "transit.station",
-		        "elementType": "labels.icon",
-		        "stylers": [
-		            {
-		                "gamma": 1
-		            },
-		            {
-		                "saturation": 50
-		            }
-		        ]
-		    }
-		],
-	};
-	var map = new google.maps.Map(document.getElementById('contact__map'), mapOptions);
 
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">RP3 Agency</h1>'+
-      '<div id="bodyContent">'+
-      '<p>7318 Wisconsin Avenue<br>Suite 450<br>Bethesda, Maryland 20814</p>'+
-      '</div>'+
-      '</div>';
-	var infowindow = new google.maps.InfoWindow({
-		content: contentString
-	});
+	if ( '' !== document.getElementById('contact__map') ) {
 
-	var marker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		title: 'RP3 Agency'
-	});
+		var myLatLng = new google.maps.LatLng(38.9827398,-77.0940005);
+		var mapOptions = {
+			center: myLatLng,
+			zoom: 11,
+			styles: [
+				{
+					"stylers": [
+						{
+							"saturation": -100
+						},
+						{
+							"gamma": 1
+						}
+					]
+				},
+				{
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "poi.business",
+					"elementType": "labels.text",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "poi.business",
+					"elementType": "labels.icon",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "poi.place_of_worship",
+					"elementType": "labels.text",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "poi.place_of_worship",
+					"elementType": "labels.icon",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"visibility": "simplified"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"stylers": [
+						{
+							"visibility": "on"
+						},
+						{
+							"saturation": 50
+						},
+						{
+							"gamma": 0
+						},
+						{
+							"hue": "#50a5d1"
+						}
+					]
+				},
+				{
+					"featureType": "administrative.neighborhood",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#333333"
+						}
+					]
+				},
+				{
+					"featureType": "road.local",
+					"elementType": "labels.text",
+					"stylers": [
+						{
+							"weight": 0.5
+						},
+						{
+							"color": "#333333"
+						}
+					]
+				},
+				{
+					"featureType": "transit.station",
+					"elementType": "labels.icon",
+					"stylers": [
+						{
+							"gamma": 1
+						},
+						{
+							"saturation": 50
+						}
+					]
+				}
+			],
+		};
+		var map = new google.maps.Map(document.getElementById('contact__map'), mapOptions);
 
-	if ( window.matchMedia( '(min-width: 600px)' ).matches ) {
-		infowindow.open(map, marker);
+		var contentString = '<div id="content">'+
+			'<div id="siteNotice">'+
+			'</div>'+
+			'<h1 id="firstHeading" class="firstHeading">RP3 Agency</h1>'+
+			'<div id="bodyContent">'+
+			'<p>7318 Wisconsin Avenue<br>Suite 450<br>Bethesda, Maryland 20814</p>'+
+			'</div>'+
+			'</div>';
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString
+		});
+
+		var marker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			title: 'RP3 Agency'
+		});
+
+		if ( window.matchMedia( '(min-width: 600px)' ).matches ) {
+			infowindow.open(map, marker);
+		}
 	}
 }
 google.maps.event.addDomListener(window, 'load', initialize);
