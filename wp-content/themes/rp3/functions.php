564b73029db9cb5a4028bf0d012a573c0dc955bc
Tracy Rotton <trotton@rp3agency.com>
2014-09-22 11:42:24 -0400

    conditionally loading google maps api script on the contact page [#497]

--- wp-content/themes/rp3/functions.php
+++ wp-content/themes/rp3/functions.php
@@ -73,14 +73,19 @@ require get_template_directory() . '/inc/widgets.php';
 function rp3_scripts() {
 	wp_enqueue_style( 'rp3-style', get_stylesheet_directory_uri() . '/css/rp3.min.css' );
 
-	wp_enqueue_script( 'rp3-javascript', get_template_directory_uri() . '/js/rp3.min.js', array( 'jquery' ), '20120206', true );
-
 	wp_enqueue_script( 'rp3-vendor', get_template_directory_uri() . '/js/rp3-vendor.min.js', array(), '20120206' );
 
 	wp_enqueue_script( 'rp3-plugins', get_template_directory_uri() . '/js/rp3-plugins.min.js', array( 'jquery' ), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
+		wp_enqueue_script( 'rp3-javascript', get_template_directory_uri() . '/js/rp3.min.js', array( 'jquery', 'rp3-google-maps' ), '20120206', true );
+	} else {
+		wp_enqueue_script( 'rp3-javascript', get_template_directory_uri() . '/js/rp3.min.js', array( 'jquery' ), '20120206', true );
+	}
+
+	if ( is_page( 'contact' ) ) {
+		wp_enqueue_script( 'rp3-google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDbQMhVNXn8QRMNJoiWJeemZ63O4NN75kI');
 	}
 }
 add_action( 'wp_enqueue_scripts', 'rp3_scripts' );
