7e1d825552c70fe08bb6eb1a74b58959b9c16c7e
Tracy Rotton <trotton@rp3agency.com>
2014-09-25 12:05:59 -0400

    adding leadership CPT [#500]

--- wp-content/plugins/rp3/inc/custom-post-types.php
+++ wp-content/plugins/rp3/inc/custom-post-types.php
@@ -84,6 +84,43 @@ function rp3_cpt_work() {
 }
 
 
+/**
+ * Leadership
+ */
+function rp3_cpt_leadership() {
+
+	// Custom Post Type
+	$args = array(
+		'labels'				=> array(
+			'name'					=> 'Leadership',
+			'singular_name'			=> 'Leadership',
+			'add_new_item'			=> 'Add New Person',
+			'edit_item'				=> 'Edit Person',
+			'new_item'				=> 'New Person',
+			'view_item'				=> 'View Person',
+			'search_items'			=> 'Search Person'
+		),
+		'description'			=> 'Agency leadership.',
+		'public'				=> true,
+		'exclude_from_search'	=> true,
+		'show_ui'				=> true,
+		'show_in_nav_menus'		=> false,
+		'show_in_menu'			=> true,
+		'show_in_admin_bar'		=> false,
+		'menu_position'			=> 20,
+		'hierarchical'			=> true,
+		'menu_icon'				=> 'dashicons-businessman',
+		'supports'				=> array( 'title', 'thumbnail' ),
+		'rewrite'				=> array(
+			'slug'					=> 'leadership',
+			'with_front'			=> false
+		)
+	);
+
+	register_post_type( 'rp3_cpt_leadership', $args );
+}
+
+
 
 
 
@@ -94,5 +131,6 @@ function rp3_cpt_work() {
 function rp3_cpt() {
 	rp3_cpt_capabilities();
 	rp3_cpt_work();
+	rp3_cpt_leadership();
 }
 add_action( 'init', 'rp3_cpt' );
