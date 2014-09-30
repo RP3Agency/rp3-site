a99ca7f91bd4ec6881d619501237fc40b7645af0
Tracy Rotton <trotton@rp3agency.com>
2014-09-30 17:55:21 -0400

    "all posts" buttons and archive template [#508]

--- wp-content/themes/rp3/content-page-news-views.php
+++ wp-content/themes/rp3/content-page-news-views.php
@@ -90,6 +90,11 @@ $blog = new WP_Query( array(
 	<?php endif; wp_reset_query(); ?>
 
 
+	<div class="all-news-link">
+		<a href="<?php echo esc_url( home_url( 'news' ) ); ?>">View All News</a>
+	</div>
+
+
 
 	<!-- Hero Image -->
 	<?php if ( '' != get_field( 'hero_image_1' ) ) :
@@ -158,6 +163,12 @@ $blog = new WP_Query( array(
 	<?php endif; wp_reset_query(); ?>
 
 
+	<div class="all-news-link">
+		<a href="<?php echo esc_url( home_url( 'blog' ) ); ?>">View All Blog Posts</a>
+	</div>
+
+
+
 	<!-- Hero Image -->
 	<?php if ( '' != get_field( 'hero_image_2' ) ) :
 		echo rp3_full_bleed_hero_image( get_field( 'hero_image_2' ), '', '', 'sub-page-hero' );
@@ -179,94 +190,3 @@ $blog = new WP_Query( array(
 
 </article>
 <!-- #post-## -->
-
-
-
-
-
-<?php /*
-
-	<section class="row articles">
-
-		<div class="blog-column">
-
-			<?php $counter = 0; if ( $blog->have_posts() ) : while ( $blog->have_posts() ) : $blog->the_post(); ?>
-
-			<?php if ( $counter !== 3 ) : ?>
-
-			<div class="row">
-
-			<?php endif; ?>
-
-				<article class="blog">
-
-					<a href="<?php echo esc_url( get_the_permalink() ); ?>">
-
-						<header class="entry-meta">
-							<div class="entry-category">Blog</div>
-
-							<?php if ( $counter < 2 ) : ?>
-								<div class="entry-featured-image"><?php the_post_thumbnail('thumb'); ?></div>
-							<?php endif; ?>
-
-							<h1 class="entry-title"><?php the_title(); ?></h1>
-						</header>
-
-						<?php the_excerpt(); ?>
-
-						<footer class="entry-meta">
-							<div><?php echo rp3_byline( 'blog', 'archive' ); ?></div>
-						</footer>
-
-					</a>
-
-				</article>
-				<!-- // .blog -->
-
-			<?php if ( $counter !== 2 ) : ?>
-
-			</div>
-			<!-- // .row -->
-
-			<?php endif; ?>
-		
-			<?php $counter++; endwhile; endif; wp_reset_query(); ?>
-
-		</div>
-		<!-- // .blog -->
-
-		<div class="news-twitter-column">
-
-			<?php $counter = 0; if ( $news->have_posts() ) : while ( $news->have_posts() ) : $news->the_post(); ?>
-
-			<article class="news">
-
-				<a href="<?php echo esc_url( get_the_permalink() ); ?>">
-
-					<header class="entry-meta">
-						<div class="entry-category">News</div>
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header>
-
-					<?php if ( $counter < 2 ) : ?>
-
-					<?php the_excerpt(); ?>
-
-					<?php endif; ?>
-
-					<footer class="entry-meta">
-						<div><?php echo get_the_date(); ?></div>
-					</footer>
-
-				</a>
-
-			</article>
-
-			<?php $counter++; endwhile; endif; wp_reset_query(); ?>
-
-		</div>
-		<!-- // .news -->
-
-	</section>
-	<!-- // .articles -->
-*/ ?>
