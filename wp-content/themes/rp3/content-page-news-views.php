d9df2becd076e7cb216f4d9b8ac92563bea25f21
Tracy Rotton <trotton@rp3agency.com>
2014-09-30 11:25:34 -0400

    more news and views page building [#508]

--- wp-content/themes/rp3/content-page-news-views.php
+++ wp-content/themes/rp3/content-page-news-views.php
@@ -12,21 +12,149 @@
 $news = new WP_Query( array(
 	'post_type'			=> 'post',
 	'category_name'		=> 'news',
-	'posts_per_page'	=> 4
+	'posts_per_page'	=> 3
 ) );
 
 $blog = new WP_Query( array(
 	'post_type'			=> 'post',
 	'category_name'		=> 'blog',
-	'posts_per_page'	=> 5
+	'posts_per_page'	=> 3
 ) );
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="entry-content">
-		<?php the_content(); ?>
-	</div><!-- .entry-content -->
+
+		<div class="entry-content__container">
+
+			<?php the_content(); ?>
+
+		</div>
+
+	</div>
+	<!-- .entry-content -->
+
+
+
+	<!-- Search form -->
+	<section class="search-form">
+
+		<?php get_search_form( true ); ?>
+
+	</section>
+	<!-- // .search-form -->
+
+
+
+
+	<!-- News Articles -->
+
+	<?php if ( $news->have_posts() ) : ?>
+
+	<section id="news-listing" class="news-listing">
+
+		<?php while ( $news->have_posts() ) : $news->the_post(); ?>
+
+			<div class="news-listing__article">
+
+				<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="block">
+
+					<h1 class="news-listing__headline"><?php the_title(); ?></h1>
+
+					<div class="news-listing__date"><?php echo get_the_date(); ?></div>
+
+					<?php the_excerpt(); ?>
+
+					<p class="link continue">Continue reading</p>
+
+				</a>
+
+			</div>
+
+		<?php endwhile; ?>
+
+	</section>
+	<!-- // #news-listing -->
+
+	<?php endif; wp_reset_query(); ?>
+
+
+
+	<!-- Hero Image -->
+	<?php if ( '' != get_field( 'hero_image_1' ) ) :
+		echo rp3_full_bleed_hero_image( get_field( 'hero_image_1' ), '', '', 'sub-page-hero' );
+	endif; ?>
+
+
+
+	<?php if ( '' != get_field( 'blog_intro_copy' ) ) : ?>
+
+	<div class="entry-content">
+
+		<div class="entry-content__container">
+
+			<?php the_field( 'blog_intro_copy' ); ?>
+
+		</div>
+
+	</div>
+	<!-- .entry-content -->
+
+	<?php endif; ?>
+
+
+
+	<!-- Blog Posts -->
+
+	<?php if ( $blog->have_posts() ) : ?>
+
+	<section id="blog-listing" class="blog-listing">
+
+		<?php while ( $blog->have_posts() ) : $blog->the_post(); ?>
+
+			<div class="blog-listing__article">
+
+				<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="block">
+
+					<h1 class="blog-listing__headline"><?php the_title(); ?></h1>
+
+					<div class="blog-listing__byline"><?php echo rp3_byline(); ?></div>
+
+					<?php the_excerpt(); ?>
+
+					<p class="link continue">Continue reading</p>
+
+				</a>
+
+			</div>
+
+
+		<?php endwhile; ?>
+
+	</section>
+	<!-- // #blog-listing -->
+
+	<?php endif; wp_reset_query(); ?>
+
+
+	<!-- Hero Image -->
+	<?php if ( '' != get_field( 'hero_image_2' ) ) :
+		echo rp3_full_bleed_hero_image( get_field( 'hero_image_2' ), '', '', 'sub-page-hero' );
+	endif; ?>
+
+
+
+	<!-- Related Content -->
+
+</article>
+<!-- #post-## -->
+
+
+
+
+
+<?php /*
 
 	<section class="row articles">
 
@@ -111,6 +239,4 @@ $blog = new WP_Query( array(
 
 	</section>
 	<!-- // .articles -->
-
-</article>
-<!-- #post-## -->
+*/ ?>
