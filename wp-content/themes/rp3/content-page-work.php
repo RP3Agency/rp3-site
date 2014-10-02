2977d00806318a3eaa23c0aaf767ea5dd45f2f58
Tracy Rotton <trotton@rp3agency.com>
2014-10-02 15:26:21 -0400

    beginning to code the work landing page, which will be hard-coded, at least for now. [#524]

---
+++ wp-content/themes/rp3/content-page-work.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package RP3
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<!-- Row 1 -->
+
+	<div id="ns365">
+		<img src="<?php echo esc_url( wp_get_attachment_url( 8636 ) ); ?>" width="1000" height="400">
+	</div>
+
+
+	<!-- Row 2 -->
+
+	<div class="container">
+
+		<div id="giftacular">
+			<img src="<?php echo esc_url( wp_get_attachment_url( 8632 ) ); ?>" width="500" height="400">
+		</div>
+
+		<div class="container">
+
+			<div class="blank">
+
+			<div id="norfolk-southern">
+				<img src="<?php echo esc_url( wp_get_attachment_url( 8635 ) ); ?>" width="250" height="200">
+			</div>
+
+		</div>
+
+		<div class="container">
+
+			<div class="gray">
+			</div>
+
+			<div class="blank">
+			</div>
+
+		</div>
+		<!-- // .container -->
+
+	</div>
+	<!-- // .container -->
+
+
+	<!-- Row 3 -->
+
+	<div id="wolf-trap">
+		<img src="<?php echo esc_url( wp_get_attachment_url( 8639 ) ); ?>" width="1000" height="400">
+	</div>
+
+
+	<!-- Row 4 -->
+
+	<div class="container">
+
+		<div class="container">
+
+			<div id="wit">
+				<img src="<?php echo esc_url( wp_get_attachment_url( 8638 ) ); ?>" width="250" height="200">
+			</div>
+
+			<div class="blank">
+			</div>
+
+		</div>
+		<!-- // .container -->
+
+		<div class="container">
+
+			<div class="blank">
+			</div>
+
+			<div id="united-way">
+				<img src="<?php echo esc_url( wp_get_attachment_url( 8637 ) ); ?>" width="250" height="200">
+			</div>
+
+		</div>
+		<!-- // .container -->
+
+		<div id="woolly">
+			<img src="<?php echo esc_url( wp_get_attachment_url( 8640 ) ); ?>" width="500" height="400">
+		</div>
+
+	</div>
+
+
+	<!-- Row 5 -->
+
+	<div id="infinite-connections">
+	</div>
+
+	<!-- Row 6 -->
+
+	<div class="container">
+
+		<div id="be-that-woman">
+			<img src="<?php echo esc_url( wp_get_attachment_url( 8631 ) ); ?>" width="500" height="400">
+		</div>
+
+		<div class="container">
+
+			<div id="norfolk-southern-2">
+				<img src="<?php echo esc_url( wp_get_attachment_url( 8634 ) ); ?>" width="250" height="200">
+			</div>
+
+			<div class="blank">
+			</div>
+
+		</div>
+
+		<div class="container">
+
+			<div class="blank">
+			</div>
+
+			<div class="gray">
+			</div>
+
+		</div>
+
+	</div>
+
+
+</article><!-- #post-## -->
