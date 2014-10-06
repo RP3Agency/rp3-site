f20bcc6e26386428d5976097af7922a2a6dde09d
Tracy Rotton <trotton@rp3agency.com>
2014-10-06 15:04:06 -0400

    changed the structure of the home page hero, but only slightly

--- wp-content/themes/rp3/components/home-page-hero.php
+++ wp-content/themes/rp3/components/home-page-hero.php
@@ -1,13 +1,11 @@
-<?php $posts = get_field( 'home_page_hero' ); ?>
+<?php $post = get_field( 'home_page_hero' ); ?>
 
-<?php if ( $posts ) : ?>
-
-	<?php foreach ( $posts as $post ) : ?>
+<?php if ( $post ) : ?>
 
 	<?php setup_postdata( $post ); ?>
 
 	<?php echo rp3_full_bleed_hero_image( get_post_thumbnail_id( get_the_ID() ), 'home-hero', get_the_permalink(), 'home-page-hero', get_the_title(), get_field( 'headline' ) ); ?>
 
-	<?php endforeach; wp_reset_postdata(); ?>
+	<?php wp_reset_postdata(); ?>
 
 <?php endif; ?>
