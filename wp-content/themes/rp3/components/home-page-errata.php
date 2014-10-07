dbb459bd4c715549c767349b843a910a480133db
Tracy Rotton <trotton@rp3agency.com>
2014-10-07 09:53:14 -0400

    more making of the home page errata tiles live [#530]

--- wp-content/themes/rp3/components/home-page-errata.php
+++ wp-content/themes/rp3/components/home-page-errata.php
@@ -1,24 +1,43 @@
+<?php
+/**
+ * Get latest blog post
+ */
+$args = array(
+	'post_type'			=> 'post',
+	'posts_per_page'	=> 1,
+	'orderby'			=> 'date',
+	'order'				=> 'DESC'
+);
+
+$blog = new WP_Query( array_merge( $args, array( 'category_name' => 'blog' ) ) );
+
+/**
+ * Get latest news post
+ */
+
+$news = new WP_Query( array_merge( $args, array( 'category_name' => 'news' ) ) );
+?>
+
 <div class="home-errata">
 
 	<div class="home-errata__row">
 
-		<div class="home-errata__widget home-errata__block">
+		<div class="home-errata__general-widget home-errata__block">
 
-			<?php dynamic_sidebar( 'home-errata__widget' ); ?>
+			<?php dynamic_sidebar( 'home-errata__general-widget' ); ?>
 
 		</div>
 		<!-- // .home-errata__widget -->
 
 		<div class="home-errata__twitter home-errata__block">
 
-			<div class="home-errata__subhead">Twitter</div>
+			<a href="https://twitter.com/rp3agency" class="block">
 
-			<p>What happens if you actually ‘like’ everything... http://ow.ly/Acy7d via @WIRED</p>
+				<?php dynamic_sidebar( 'home-errata__twitter-widget' ); ?>
 
-			<div class="home-errata__date">August 14, 2014</div>
+			</a>
 
 		</div>
-
 
 	</div>
 	<!-- // .home-errata__row -->
@@ -29,6 +48,7 @@
 		<div class="home-errata__sandbox home-errata__block">
 			Sandbox
 		</div>
+
 
 		<div class="home-errata__blog--gray home-errata__block">
 
@@ -46,26 +66,50 @@
 
 	<div class="home-errata__row">
 
-		<div class="home-errata__blog--green home-errata__block">
+		<?php if ( $blog->have_posts() ) : ?>
 
-			<div class="home-errata__subhead">Blog</div>
+			<?php while ( $blog->have_posts() ) : $blog->the_post(); ?>
 
-			<h1 class="home-errata__header">You're Doing It Wrong.</h1>
+				<div class="home-errata__blog--green home-errata__block">
 
-			<div class="home-errata__date">August 14, 2014</div>
+					<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="block">
 
-		</div>
+						<div class="home-errata__subhead">Blog</div>
+
+						<h1 class="home-errata__header"><?php the_title(); ?></h1>
+
+						<div class="home-errata__date"><?php echo get_the_date(); ?></div>
+
+					</a>
+
+				</div>
+
+			<?php endwhile; wp_reset_query(); ?>
+
+		<?php endif; ?>
 
 
-		<div class="home-errata__news home-errata__block">
+		<?php if ( $news->have_posts() ) : ?>
 
-			<div class="home-errata__subhead">News</div>
+			<?php while ( $news->have_posts() ) : $news->the_post(); ?>
 
-			<h1 class="home-errata__header">RP3 Gains Four</h1>
+				<div class="home-errata__news home-errata__block">
 
-			<div class="home-errata__date">August 14, 2014</div>
+					<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="block">
 
-		</div>
+						<div class="home-errata__subhead">News</div>
+
+						<h1 class="home-errata__header"><?php the_title(); ?></h1>
+
+						<div class="home-errata__date"><?php echo get_the_date(); ?></div>
+
+					</a>
+
+				</div>
+
+			<?php endwhile; wp_reset_query(); ?>
+
+		<?php endif; ?>
 
 	</div>
 	<!-- // .home-errata__row -->
