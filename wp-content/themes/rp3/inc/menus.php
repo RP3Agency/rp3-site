9eb20ba28e4bdc9f5117a9cd023f63d01e780115
Tracy Rotton <trotton@rp3agency.com>
2014-10-08 11:48:45 -0400

    "about" page submenu now obsolete [#551]

--- wp-content/themes/rp3/inc/menus.php
+++ wp-content/themes/rp3/inc/menus.php
@@ -4,7 +4,6 @@ function rp3_register_nav_menus() {
 
 	register_nav_menus( array(
 		'primary'			=> __( 'Primary Menu', 'rp3' ),
-		'about-page'		=> __( 'About Page Submenu', 'rp3' ),
 		'footer-social'		=> __( 'Footer Social', 'rp3' ),
 		'contact-social'	=> __( 'Contact Page Social', 'rp3' )
 	) );
