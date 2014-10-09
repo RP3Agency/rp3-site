c3dcf2d008a76cb5f75d7f0b81638f38462c8d3f
Tracy Rotton <trotton@rp3agency.com>
2014-10-09 13:40:39 -0400

    typo fix

--- wp-content/themes/rp3/footer.php
+++ wp-content/themes/rp3/footer.php
@@ -70,7 +70,7 @@
 			<!-- // .site-footer__social -->
 
 			<div class="site-footer__copyright">
-				<span class="copyright">© <?php echo date('Y'); ?> RP3 Agency, <a href="http://www.aaaa.org/news/agency/Pages/RP3AgencyReceivesCertificationAsaWomen-OwnedSmallBusiness.aspx">A Certified Women-Owned Small Business</a></span>
+				<span class="copyright">© <?php echo date('Y'); ?> RP3 Agency, <a href="http://www.aaaa.org/news/agency/Pages/RP3AgencyReceivesCertificationAsaWomen-OwnedSmallBusiness.aspx">A Certified Woman-Owned Small Business</a></span>
 			</div>
 			<!-- // .copy -->
 
