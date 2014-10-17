39fd6f768960893f51f337d2ebd7a6f30e41015c
Tracy Rotton <trotton@rp3agency.com>
2014-10-17 09:03:03 -0400

    created the template part and styling for the 2x3 image grid [#606]

--- wp-content/themes/rp3/components/work/images-grid.php
+++ wp-content/themes/rp3/components/work/images-grid.php
@@ -2,7 +2,11 @@
 
 	<?php while ( have_rows( 'mini_study_images' ) ) : the_row(); ?>
 
-		<img src="<?php echo esc_url( wp_get_attachment_url( get_sub_field( 'mini_study_image' ) ) ); ?>">
+		<div class="work-images--grid__image">
+
+			<img src="<?php echo esc_url( wp_get_attachment_url( get_sub_field( 'mini_study_image' ) ) ); ?>">
+
+		</div>
 
 	<?php endwhile; ?>
 
