8ac86b1fbb24d3b7a132e7b8a93c4e2fb1414fc1
Tracy Rotton <trotton@rp3agency.com>
2014-11-21 12:55:59 -0500

    pulling theme template files out of wp-content and into src/theme. nothing in wp-content (in general) will be committed to git [#695]

--- wp-content/themes/rp3/bak/searchform-header-aux.php
+++
@@ -1,8 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ); ?>">
-	</label>
-	<button type="submit" class="search-submit"><?php echo __( 'Search', 'rp3' ); ?></button>
-	<a href="#!" class="close">Close</a>
-</form>
