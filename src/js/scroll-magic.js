ca4defea2ed8f8caee23939c020d2e98b0cf5c08
Tracy Rotton <trotton@rp3agency.com>
2015-01-08 11:33:13 -0500

    further progress on the scroll magic experiment [#612]

--- src/js/scroll-magic.js
+++ src/js/scroll-magic.js
@@ -12,11 +12,33 @@ jQuery(document).ready( function($) {
 	});
 
 	// build scenes
-	new ScrollScene({
-			triggerElement: "#home-work",
-			reverse: false
-		})
-		.setClassToggle( "#home-work .hero:first-child", "active" )
-		.addTo( controller );
+	var $hero = $('#home-work .hero');
+
+	if ( $hero.length > 0 ) {
+		$hero.each( function(i) {
+
+			var j = i + 1;
+
+			var targetEl		= '#home-work .hero:nth-child(' + j + ')',
+				targetClass		= 'active active-' + j;
+
+			new ScrollScene({
+				triggerElement:		"#home-work",
+				reverse:			false
+			})
+			.setClassToggle( targetEl, targetClass )
+			.addTo( controller );
+		});
+
+		// Make sure our container has the proper height
+		$("#home-work").height( 600 * $hero.length - $hero.length );
+	}
+
+	// new ScrollScene({
+	// 		triggerElement: "#home-work .hero",
+	// 		reverse: false
+	// 	})
+	// 	.setClassToggle( "#home-work .hero", "active" )
+	// 	.addTo( controller );
 
 }(jQuery));
