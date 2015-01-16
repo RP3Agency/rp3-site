bdbc57f051612a5c255bd7114cf2a3a156459ca5
Tracy Rotton <trotton@rp3agency.com>
2015-01-16 08:49:45 -0500

    creating a new panel for the hero section of the work pages [#708]

---
+++ src/theme/panels/hero_image.php
@@ -0,0 +1,33 @@
+<?php
+global $class;
+?>
+
+<?php if ( '' != get_sub_field( 'video_link' ) ) : ?>
+
+	<div class="video__trigger">
+
+		<a href="<?php echo esc_url( get_sub_field( 'video_link' ) ); ?>" id="video__trigger" class="block">
+
+			<?php echo rp3_full_bleed_hero_image( get_sub_field( 'image' ), array(
+				'image_size'	=> 'case-study',
+				'classes'		=> 'hero-image ' . $class . '-hero-image case-study-hero-image'
+			) ); ?>
+
+		</a>
+
+		<div class="video__modal" id="video__modal">
+
+			<?php echo wp_oembed_get( get_sub_field( 'video_link' ) ); ?>
+
+		</div>
+
+	</div>
+
+<?php else: ?>
+
+	<?php echo rp3_full_bleed_hero_image( get_sub_field( 'image' ), array(
+		'image_size'	=> 'case-study',
+		'classes'		=> 'hero-image ' . $class . '-hero-image case-study-hero-image'
+	) ); ?>
+
+<?php endif; ?>
