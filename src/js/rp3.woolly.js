c763d8ea7037e8d08ac894b25a6a932d329b9009
Tracy Rotton <trotton@rp3agency.com>
2015-01-17 14:30:02 -0500

    first stab at getting the positioning right, and it's mostly working [#717]

--- src/js/rp3.woolly.js
+++ src/js/rp3.woolly.js
@@ -37,6 +37,42 @@ rp3.woolly = (function($) {
 		}
 	},
 
+	// Adjust the positioning as the viewport changes
+	positioning = function() {
+
+		var $el1 = $homeHeroImage.find('.position-1'),
+			$el2 = $homeHeroImage.find('.position-2'),
+			$el3 = $homeHeroImage.find('.position-3'),
+
+			rightPos1 = 0,
+			leftPos2  = 0,
+			leftPos3  = 0,
+			topPos1   = 0,
+			topPos2   = 0,
+			topPos3   = 0,
+
+			halfWidth  = Math.floor( $(window).width() / 2 ),
+			halfHeight = Math.floor( $homeHeroImage.height() / 2 );
+
+		// Apply the proper offsets to each
+		rightPos1 = halfWidth + 546;
+		leftPos2  = halfWidth + 489;
+		leftPos3  = halfWidth + 99;
+
+		topPos1   = halfHeight - 208;
+		topPos2   = halfHeight - 535;
+		topPos3   = halfHeight + 129;
+
+		// Now position our elements accordingly
+		$el1.css('right', rightPos1 + 'px');
+		$el2.css('left', leftPos2 + 'px');
+		$el3.css('left', leftPos3 + 'px');
+
+		$el1.css('top', topPos1 + 'px');
+		$el2.css('top', topPos2 + 'px');
+		$el3.css('top', topPos3 + 'px');
+	},
+
 	initialize = function() {
 
 		if ( $homeHeroImage.length > 0 ) {
@@ -46,6 +82,8 @@ rp3.woolly = (function($) {
 				posterElements[i] = $('<div>').addClass( 'woolly position-' + j + ' ' + posters[i] );
 				$homeHeroImage.append( posterElements[i] );
 			}
+
+			positioning();
 		}
 	},
 
@@ -53,6 +91,10 @@ rp3.woolly = (function($) {
 		initialize();
 
 		setInterval( animation, 3000 );
+
+		$(window).on('resize', function() {
+			positioning();
+		});
 	};
 
 	return {
@@ -64,7 +106,7 @@ rp3.woolly = (function($) {
 (function() {
 	'use strict';
 
-	if ( ! Modernizr.touch ) {
+	if ( Modernizr.csstransforms ) {
 		rp3.woolly.init();
 	}
 }());
