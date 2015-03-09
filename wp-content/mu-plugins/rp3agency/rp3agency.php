6874606e99f5d58fbc54feac27aaf266a1984988
Tracy Rotton <trotton@rp3agency.com>
2015-03-09 08:39:20 -0400

    [#mu-plugins] first attempt at sorting out a viable mu-plugins system

---
+++ wp-content/mu-plugins/rp3agency/rp3agency.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: RP3 Agency
+Description: Custom post types and taxonomies and other custom functionality used by RP3 Agency.
+Version: 3.1
+Author: RP3 Agency
+Author URI: http://rp3agency.com
+*/
+
+define( 'RP3_PLUGIN_DIR', plugin_dir_path( __FILE__ ) . '/rp3agency' );
+define( 'RP3_PLUGIN_INC_DIR', RP3_PLUGIN_DIR . '/inc' );
+
+require_once( RP3_PLUGIN_INC_DIR . '/custom-post-types.php' );
+require_once( RP3_PLUGIN_INC_DIR . '/home-page-featured.php' );
