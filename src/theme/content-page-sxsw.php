1eaa4efbea2e90a8167b7b6325137960b16351bf
Tracy Rotton <trotton@rp3agency.com>
2015-04-15 13:52:51 -0400

    Hotfix: fixing classes for SXSW blog listing [#2.13.2.1]

--- src/theme/content-page-sxsw.php
+++ src/theme/content-page-sxsw.php
@@ -30,15 +30,15 @@ $blog = new WP_Query( array(
 
 	<?php if ( $blog->have_posts() ) : ?>
 
-	<section id="blog-listing" class="blog-listing" data-paged="<?php echo esc_attr( $paged ); ?>">
+	<section id="blog-listing" class="listing" data-paged="<?php echo esc_attr( $paged ); ?>">
 
 		<?php while ( $blog->have_posts() ) : $blog->the_post(); ?>
 
-			<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="block blog-listing__article">
+			<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="block listing__article">
 
 				<?php if ( '' != get_the_post_thumbnail() ) : ?>
 
-					<div class="blog-listing__thumbnail">
+					<div class="listing__thumbnail">
 
 						<?php echo get_the_post_thumbnail( get_the_ID(), 'news-blog-thumbnail' ); ?>
 
@@ -46,11 +46,11 @@ $blog = new WP_Query( array(
 
 				<?php endif; ?>
 
-				<h1 class="blog-listing__headline"><?php the_title(); ?></h1>
+				<h1 class="listing__headline"><?php the_title(); ?></h1>
 
-				<div class="blog-listing__byline"><?php echo rp3_byline(); ?></div>
+				<div class="listing__byline"><?php echo rp3_byline(); ?></div>
 
-				<div class="blog-listing__excerpt">
+				<div class="listing__excerpt">
 
 					<?php echo the_excerpt(); ?>
 
