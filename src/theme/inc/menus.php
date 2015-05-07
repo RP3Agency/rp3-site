336a98ade6ce540ff2b9c55ae29d94353da8edc1
Tracy Rotton <trotton@rp3agency.com>
2015-05-07 12:15:24 -0400

    creating a new menu to use in the footer [#847]

--- src/theme/inc/menus.php
+++ src/theme/inc/menus.php
@@ -6,7 +6,8 @@ function rp3_register_nav_menus() {
 		'primary'			=> __( 'Primary Menu', 'rp3' ),
 		'footer-social'		=> __( 'Footer Social', 'rp3' ),
 		'contact-social'	=> __( 'Contact Page Social', 'rp3' ),
-		'auxiliary'			=> __( 'Auxiliary Menu', 'rp3' )
+		'auxiliary'			=> __( 'Auxiliary Menu', 'rp3' ),
+		'footer'			=> __( 'Footer', 'rp3' )
 	) );
 }
 add_action( 'init', 'rp3_register_nav_menus' );
