78a33a0c117eb342cf2af31ece458f689640be23
Tracy Rotton <trotton@rp3agency.com>
2015-08-14 09:58:13 -0400

    [#refactor]

---
+++ src/theme/template-parts/component-blog-interstitial.php
@@ -0,0 +1,17 @@
+<section class="site-blog__interstitial">
+
+	<div class="site-blog__interstitial__container">
+
+		<div class="site-blog__interstitial__wrapper">
+
+			<div class="site-blog__interstitial__content">
+
+				<p>RP3's <strong>Building Opportunity</strong> blog showcases our ongoing exploration of new ideas, new technologies and new experiences that propel businesses forward. If you like what you see, we invite you to learn more about <a href="<?php echo esc_url( home_url( '/agency/' ) ); ?>">RP3</a>, the DC-based creative agency behind all the thinking.</p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</section>
--- src/theme/template-parts/content-single-blog.php
+++ src/theme/template-parts/content-single-blog.php
@@ -23,11 +23,11 @@
 			<section class="single-blog__featured-image">
 
 				<?php
-				$image['small'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'eight_three_small' );
-				$image['small_2x'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'eight_three_small_2x' );
+				$image['small'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'four_three_small' );
+				$image['small_2x'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'four_three_small_2x' );
 
-				$image['medium'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'eight_three_medium' );
-				$image['medium_2x'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'eight_three_medium_2x' );
+				$image['medium'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'four_three_medium' );
+				$image['medium_2x'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'four_three_medium_2x' );
 
 				$image['large'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'eight_three_large' );
 				$image['large_2x'] = wp_get_attachment_image_src( get_post_thumbnail_id(), 'eight_three_large_2x' );
