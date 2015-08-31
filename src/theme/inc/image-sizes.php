436228e26e3ff7a6c6d699e202f0f50d0f3fa247
Tracy Rotton <trotton@rp3agency.com>
2015-08-31 11:22:16 -0400

    [#964]

--- src/theme/inc/image-sizes.php
+++ src/theme/inc/image-sizes.php
@@ -53,6 +53,22 @@ function rp3_image_sizes() {
 
 	add_image_size( 'three_four_large', 1200 * 0.75, 1200, true );
 	add_image_size( 'three_four_large_2x', 1200 * 0.75 * 2, 1200 * 2, true );
+
+
+	/* ==========================================================================
+	   16:9
+	   ========================================================================== */
+
+	add_image_size( 'sixteen_nine_thumb', 320 / 2, 320 * 0.5625 / 2, true );
+
+	add_image_size( 'sixteen_nine_small', 320, 320 * 0.5625, true );
+	add_image_size( 'sixteen_nine_small_2x', 320 * 2, 320 * 0.5625 * 2, true );
+
+	add_image_size( 'sixteen_nine_medium', 600, 600 * 0.5625, true );
+	add_image_size( 'sixteen_nine_medium_2x', 600 * 2, 600 * 0.5625 * 2, true );
+
+	add_image_size( 'sixteen_nine_large', 1200, 1200 * 0.5625, true );
+	add_image_size( 'sixteen_nine_large_2x', 1200 * 2, 1200 * 0.5625 * 2, true );
 }
 
 add_action( 'init', 'rp3_image_sizes' );
@@ -64,7 +80,8 @@ function rp3_custom_image_size( $sizes ) {
 	return array_merge( $sizes, array(
 		'eight_three_large' => '8:3',
 		'four_three_large' => '4:3',
-		'three_four_large' => '3:4'
+		'three_four_large' => '3:4',
+		'sixteen_nine_large' => '16:9'
 	) );
 }
 
--- src/theme/panels/video-panel.php
+++ src/theme/panels/video-panel.php
@@ -16,7 +16,23 @@
 
 			<?php if ( '' != get_sub_field( 'full-width-image' ) ) : ?>
 
-				<?php echo rp3_picture_element_v2( esc_attr( get_sub_field( 'full-width-image' ) ), 'video-panel' ); ?>
+				<?php
+				$image['small'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_small' );
+				$image['small_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_small_2x' );
+
+				$image['medium'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_medium' );
+				$image['medium_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_medium_2x' );
+
+				$image['large'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_large' );
+				$image['large_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_large_2x' );
+				?>
+
+				<picture>
+					<source srcset="<?php echo esc_url( $image['large'][0] ); ?>, <?php echo esc_url( $image['large_2x'][0] ); ?> 2x" media="(min-width: 37.5rem)" />
+					<source srcset="<?php echo esc_url( $image['medium'][0] ); ?>, <?php echo esc_url( $image['medium_2x'][0] ); ?> 2x" media="(min-width: 20.0625rem)" />
+					<source srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
+					<img srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
+				</picture>
 
 			<?php endif; ?>
 
