8e98cd4d17fa3e045828cc9ffc66dd37237e02c8
Tracy Rotton <trotton@rp3agency.com>
2015-09-05 08:18:06 -0400

    [#1084] employing some CSS tricks to get our legacy images looking in the correct proportions

--- src/theme/yarpp-template-blog.php
+++ src/theme/yarpp-template-blog.php
@@ -33,14 +33,16 @@ if ( have_posts() ) :
 					$image['large_2x'] = wp_get_attachment_image_src( $blog_image_id, 'four_three_small_2x' );
 					?>
 
-					<picture>
-						<source srcset="<?php echo esc_url( $image['large'][0] ); ?>, <?php echo esc_url( $image['large_2x'][0] ); ?> 2x" media="(min-width: 37.5rem)" />
-						<source srcset="<?php echo esc_url( $image['medium'][0] ); ?>, <?php echo esc_url( $image['medium_2x'][0] ); ?> 2x" media="(min-width: 20.0625rem)" />
-						<source srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
-						<img srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
-					</picture>
+					<div class="single-blog__related__post__featured-image">
 
+						<picture>
+							<source srcset="<?php echo esc_url( $image['large'][0] ); ?>, <?php echo esc_url( $image['large_2x'][0] ); ?> 2x" media="(min-width: 37.5rem)" />
+							<source srcset="<?php echo esc_url( $image['medium'][0] ); ?>, <?php echo esc_url( $image['medium_2x'][0] ); ?> 2x" media="(min-width: 20.0625rem)" />
+							<source srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
+							<img srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
+						</picture>
 
+					</div>
 
 					<div class="single-blog__related__post__label">
 
