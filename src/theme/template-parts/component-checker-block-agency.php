57d5880decf2b0f4b3b93c5fb09e5c39db5e2599
Tracy Rotton <trotton@rp3agency.com>
2015-09-08 12:11:46 -0400

    [#1043] checker blocks for the agency page

--- src/theme/template-parts/component-checker-block-agency.php
+++ src/theme/template-parts/component-checker-block-agency.php
@@ -1,42 +1,49 @@
+<section id="checker-block" class="checker-block checker-block--agency">
 
+	<div class="checker-block__inner">
 
-<section id="checker-block" class="checker-block">
-	<div class="checker-block__block"> 
-		<span class="checker-block__block-segment--text block-segment">This the agency page checker block component</span>
-		<span class="checker-block__block-segment--img block-segment"></span>
-		<span class="checker-block__block-segment--blue block-segment"></span>
+		<div class="checker-block__block checker-block__block--01">
+			<img srcset="<?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-01.jpg' ); ?>, <?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-01@2x.jpg' ); ?> 2x" alt=""/>
+		</div>
+
+		<div class="checker-block__block checker-block__block--02">
+			<img srcset="<?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-02.jpg' ); ?>, <?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-02@2x.jpg' ); ?> 2x" alt=""/>
+		</div>
+
+		<div class="checker-block__block checker-block__block--03">
+			<img srcset="<?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-03.jpg' ); ?>, <?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-03@2x.jpg' ); ?> 2x" alt=""/>
+		</div>
+
+		<div class="checker-block__block checker-block__block--04">
+			<img srcset="<?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-04.jpg' ); ?>, <?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-04@2x.jpg' ); ?> 2x" alt=""/>
+		</div>
+
+		<div class="checker-block__block checker-block__block--05">
+			<img srcset="<?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-05.jpg' ); ?>, <?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-05@2x.jpg' ); ?> 2x" alt=""/>
+		</div>
+
+		<div class="checker-block__block checker-block__block--06">
+			<img srcset="<?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-06.jpg' ); ?>, <?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-06@2x.jpg' ); ?> 2x" alt=""/>
+		</div>
+
+		<div class="checker-block__block checker-block__block--07">
+			<img srcset="<?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-07.jpg' ); ?>, <?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-07@2x.jpg' ); ?> 2x" alt=""/>
+		</div>
+
+		<div class="checker-block__block checker-block__block--08">
+			<img srcset="<?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-08.jpg' ); ?>, <?php echo esc_url( get_template_directory_uri() . '/images/checker-block-agency/checker-block-agency-08@2x.jpg' ); ?> 2x" alt=""/>
+		</div>
+
+		<!-- Blue Spacer Blocks -->
+
+		<div class="checker-block__block checker-block__block--blue-01"></div>
+
+		<div class="checker-block__block checker-block__block--blue-02"></div>
+
+		<!-- Magenta Spacer Block -->
+
+		<div class="checker-block__block checker-block__block--magenta"></div>
+
 	</div>
 
-	<div class="checker-block__block"> 
-		<span class="checker-block__block-segment--img block-segment"></span>
-		<span class="checker-block__block-segment--img block-segment second"></span>
-		<span class="checker-block__block-segment--img block-segment second"></span>
-		<span class="checker-block__block-segment--img block-segment"></span>
-	</div>
-
-	<div class="checker-block__block"> 
-		<span class="checker-block__block-segment--blue block-segment"></span>
-		<span class="checker-block__block-segment--img block-segment"></span>
-		<span class="checker-block__block-segment--img block-segment"></span>
-		<span class="checker-block__block-segment--img block-segment second"></span>
-	</div>
-
-	<div class="checker-block__block">
-		<span class="checker-block__block-segment--img block-segment second"></span>
-		<span class="checker-block__block-segment--img block-segment"></span>
-		<span class="checker-block__block-segment--magenta block-segment"></span>
-		<span class="checker-block__block-segment--img block-segment"></span>
-	</div>
-
-	<div class="checker-block__block"> 
-		<span class="checker-block__block-segment--magenta block-segment"></span>
-		<span class="checker-block__block-segment--img block-segment"></span>
-		<span class="checker-block__block-segment--text block-segment">This the agency page checker block component</span>
-	</div>
-
-	<div class="checker-block__block"> 
-		<span class="checker-block__block-segment--text block-segment">This the agency page checker block component</span>
-		<span class="checker-block__block-segment--img block-segment"></span>
-		<span class="checker-block__block-segment--blue block-segment"></span>
-	</div>
 </section>
