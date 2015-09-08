9c3bcc73bc5acb6c2e15885a4195ffc93dd9c4d0
Tracy Rotton <trotton@rp3agency.com>
2015-09-08 09:43:35 -0400

    [#1065] new video play button

---
+++ src/theme/template-parts/inline-icon-video-play.svg.php
@@ -0,0 +1,8 @@
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 38.2 38.2" enable-background="new 0 0 38.2 38.2" xml:space="preserve">
+<g>
+	<rect x="0.7" y="0.7" fill="none" stroke="#00ADEE" stroke-width="1.4" stroke-miterlimit="10" width="36.8" height="36.8"/>
+	<polygon fill="none" stroke="#00ADEE" stroke-width="1.4" stroke-miterlimit="10" points="10.6,19.1 10.6,7.9 20.3,13.5 30,19.1 
+		20.3,24.7 10.6,30.3 	"/>
+</g>
+</svg>
