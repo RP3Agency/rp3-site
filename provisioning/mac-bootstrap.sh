4108791a50f347696c6d9c31841151fe1265ace9
Tracy Rotton <trotton@rp3agency.com>
2015-10-19 18:52:32 -0400

    updates to the Vagrant file and provisioning, but you still need to pull down a database dump from WP Migrate DB Pro

--- provisioning/mac-bootstrap.sh
+++
@@ -1,4 +0,0 @@
-# Everyone should have brew installed to begin with
-brew install terminal-notifier
-brew linkapps
-/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier -title "$2" -message "$3"
