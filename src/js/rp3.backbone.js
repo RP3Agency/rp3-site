fa1967af334883f9dca6a23024072e7c40225b84
Tracy Rotton <trotton@rp3agency.com>
2015-10-25 15:27:17 -0400

    Added "thumbnail" as a featured image size option

--- src/js/rp3.backbone.js
+++ src/js/rp3.backbone.js
@@ -21,6 +21,7 @@ rp3.backbone = (function($, _, Backbone, wp) {
 				'four_three_medium_2x':		null,
 				'eight_three_large':		null,
 				'eight_three_large_2x':		null,
+				'thumbnail':				null
 			},
 
 			// set calculated values on model initialization
@@ -115,6 +116,9 @@ rp3.backbone = (function($, _, Backbone, wp) {
 
 				case 'eight_three_large_2x':
 					return defaultImage + '.jpg';
+
+				case 'thumbnail':
+					return defaultImage + '-150x150.jpg';
 
 				default:
 					return false;
