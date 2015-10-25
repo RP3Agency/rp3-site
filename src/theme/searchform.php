9e95199cc473f88ec4c0d14ced4dceb887b08ea5
Tracy Rotton <trotton@rp3agency.com>
2015-10-25 08:47:15 -0400

    added an ID to the search form because javascript

--- src/theme/searchform.php
+++ src/theme/searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" id="search-form" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ); ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ); ?>" />
