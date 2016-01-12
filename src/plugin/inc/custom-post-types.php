e36f16d2b35d29993f5d732f05a657f7d921b49f
Tracy Rotton <trotton@rp3agency.com>
2016-01-12 16:44:02 -0500

    removed the capabilities cpt

--- src/plugin/inc/custom-post-types.php
+++ src/plugin/inc/custom-post-types.php
@@ -6,47 +6,6 @@
 
 
 /**
- * Capabilities:
- * Tweet-sized capabilities for the "About" page
- */
-function rp3_cpt_capabilities() {
-
-	// Custom Post Type
-	$args = array(
-		'labels'				=> array(
-			'name'					=> 'Capabilities',
-			'singular_name'			=> 'Capability'
-		),
-		'description'			=> 'Tweet-sized capabilities information for the "About" page.',
-		'public'				=> true,
-		'exclude_from_search'	=> true,
-		'publicly_queryable'	=> false,
-		'show_ui'				=> true,
-		'show_in_nav_menus'		=> false,
-		'show_in_menu'			=> true,
-		'show_in_admin_bar'		=> false,
-		'menu_position'			=> 20,
-		'hierarchical'			=> true,
-		'supports'				=> array( 'title', 'page-attributes' ),
-		'taxonomies'			=> array( 'rp3_capabilities_types' )
-	);
-
-	register_post_type( 'rp3_capabilities', $args );
-
-	// Custom Taxonomy
-	$args = array(
-		'labels'				=> array(
-			'name'					=> 'Capability Types',
-			'singular_name'			=> 'Capability Type'
-		),
-		'hierarchical'			=> true
-	);
-
-	register_taxonomy( 'rp3_capabilities_types', 'rp3_capabilities', $args );
-}
-
-
-/**
  * Work
  * Work highlights and case studies.
  */
@@ -258,15 +217,10 @@ function rp3_blog_taxonomies() {
 }
 
 
-
-
-
-
 /**
  * Activate all the custom post types and taxonomies in one fell swoop
  */
 function rp3_cpt() {
-	// rp3_cpt_capabilities();
 	rp3_cpt_work();
 	rp3_cpt_leadership();
 	rp3_cpt_news();
