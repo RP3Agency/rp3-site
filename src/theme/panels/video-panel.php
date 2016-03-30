8fd8b311b917504dbdf9fdbfc0968e3f94ec8816
Tracy Rotton <tracy@taupecat.com>
2016-03-30 12:25:54 -0400

    [#1243] made the work videos use the ACF oEmbed field, just like I did on the home page.

--- src/theme/panels/video-panel.php
+++ src/theme/panels/video-panel.php
@@ -7,65 +7,97 @@
 
 /** Get the video embed code and assign an ID to it */
 
-// Create an id off of the MD5 checksum of the Vimeo source string
-$hash = md5( get_sub_field( 'video_link' ) );
+if ( '' !== get_sub_field( 'video_link' ) ) {
 
-$embed_args = array(
-    'title' => 0,
-    'byline' => 0,
-    'portrait' => 0,
-    'player_id' => $hash . '__iframe',
-);
-$iframe = wp_oembed_get( get_sub_field( 'video_link' ), $embed_args );
+	$video = get_sub_field( 'video_link' );
+
+	preg_match( '/src="(.+?)"/', $video, $matches );
+	$src = $matches[1];
+
+	// Create an id off of the MD5 checksum of the Vimeo source string
+
+	$hash = md5( $src );
+
+	// Add additional parameters for autoplay and the API
+
+	$params = array(
+		'title'			=> 0,
+		'byline'		=> 0,
+		'portrait'		=> 0,
+		'player_id'		=> $hash . '__iframe'
+	);
+
+	$new_src = add_query_arg( $params, $src );
+
+	$video = str_replace( $src, $new_src, $video );
+
+	// Add the ID to the iframe
+
+	$attributes = 'id="' . $hash . '__iframe"';
+
+	$video = str_replace( '></iframe>', ' ' . $attributes . '></iframe>', $video );
+}
 ?>
 
 <!-- Video Panel -->
 
-<section id="<?php echo esc_attr( $hash ); ?>" class="video-panel panel video-panel__trigger">
+<?php if ( ( '' !== get_sub_field( 'video_link' ) ) || ( '' != get_sub_field( 'full-width-image' ) ) ) : ?>
 
-	<a href="#!" id="<?php echo esc_attr( $hash . '__trigger' ); ?>" data-id="<?php echo esc_attr( $hash ); ?>" class="video-panel__image video__trigger block">
+	<section id="<?php echo esc_attr( $hash ); ?>" class="video-panel panel video-panel__trigger">
 
-		<div class="video-panel__image__content">
+		<a href="#!" id="<?php echo esc_attr( $hash . '__trigger' ); ?>" data-id="<?php echo esc_attr( $hash ); ?>" class="video-panel__image video__trigger block">
 
 			<?php if ( '' != get_sub_field( 'full-width-image' ) ) : ?>
 
-				<?php
-				$image['small'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_small' );
-				$image['small_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_small_2x' );
+				<div class="video-panel__image__content">
 
-				$image['medium'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_medium' );
-				$image['medium_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_medium_2x' );
+					<?php
+					$image['small'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_small' );
+					$image['small_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_small_2x' );
 
-				$image['large'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_large' );
-				$image['large_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_large_2x' );
-				?>
+					$image['medium'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_medium' );
+					$image['medium_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_medium_2x' );
 
-				<picture>
-					<source srcset="<?php echo esc_url( $image['large'][0] ); ?>, <?php echo esc_url( $image['large_2x'][0] ); ?> 2x" media="(min-width: 37.5rem)" />
-					<source srcset="<?php echo esc_url( $image['medium'][0] ); ?>, <?php echo esc_url( $image['medium_2x'][0] ); ?> 2x" media="(min-width: 20.0625rem)" />
-					<source srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
-					<img srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
-				</picture>
+					$image['large'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_large' );
+					$image['large_2x'] = wp_get_attachment_image_src( get_sub_field( 'full-width-image' ), 'sixteen_nine_large_2x' );
+					?>
+
+					<picture>
+						<source srcset="<?php echo esc_url( $image['large'][0] ); ?>, <?php echo esc_url( $image['large_2x'][0] ); ?> 2x" media="(min-width: 37.5rem)" />
+						<source srcset="<?php echo esc_url( $image['medium'][0] ); ?>, <?php echo esc_url( $image['medium_2x'][0] ); ?> 2x" media="(min-width: 20.0625rem)" />
+						<source srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
+						<img srcset="<?php echo esc_url( $image['small'][0] ); ?>, <?php echo esc_url( $image['small_2x'][0] ); ?> 2x" />
+					</picture>
+
+				</div>
+				<!-- video-panel image content -->
 
 			<?php endif; ?>
 
-		</div>
-		<!-- video-panel image content -->
+			<?php if ( '' !== get_sub_field( 'video_link' ) ) : ?>
 
-		<div class="video-panel__play-button">
+				<div class="video-panel__play-button">
 
-			<?php get_template_part( 'template-parts/inline', 'icon-video-play.svg' ); ?>
+					<?php get_template_part( 'template-parts/inline', 'icon-video-play.svg' ); ?>
 
-		</div>
+				</div>
 
-	</a>
-	<!-- video-panel image -->
+			<?php endif; ?>
 
-	<div id="<?php echo esc_attr( $hash . '__modal' ); ?>" class="video-panel__modal">
+		</a>
+		<!-- video-panel image -->
 
-		<?php echo $iframe; ?>
+		<?php if ( '' !== get_sub_field( 'video_link' ) ) : ?>
 
-	</div>
+			<div id="<?php echo esc_attr( $hash . '__modal' ); ?>" class="video-panel__modal">
 
-</section>
-<!-- full-width-image-panel -->
+				<?php echo $video; ?>
+
+			</div>
+
+		<?php endif; ?>
+
+	</section>
+	<!-- full-width-image-panel -->
+
+<?php endif; ?>
