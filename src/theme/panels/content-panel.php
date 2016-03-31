637258305688fffaca269676d302733bdb49a369
Tracy Rotton <tracy@taupecat.com>
2016-03-31 11:13:38 -0400

    [#1246] removed some commented-out code

--- src/theme/panels/content-panel.php
+++ src/theme/panels/content-panel.php
@@ -4,14 +4,6 @@
  *
  * @package RP3
  */
-
-/** If we're on the results content block, check for tags */
-// if ( get_sub_field( 'display-tags' ) ) {
-// 	$terms = get_the_terms( get_the_ID(), 'rp3_tax_work_tags' );
-// 	foreach ( $terms as $term ) {
-// 		$work_tags[] = $term->name;
-// 	}
-// }
 ?>
 
 <!-- Content Panel -->
@@ -39,85 +31,3 @@
 	</section>
 
 <?php endif; ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php /*
-	<!-- Content -->
-
-	<div class="content-panel__content">
-
-			<div class="content-panel__content__inner">
-
-				<?php the_sub_field( 'content' ); ?>
-
-			</div>
-			<!-- content-panel content inner -->
-
-		<?php endif; ?>
-
-	</div>
-	<!-- content-panel content -->
-
-
-	<!-- Tags -->
-
-	<?php if ( ( get_sub_field( 'display-tags' ) ) && ( isset( $work_tags ) ) && ( 0 < count( $work_tags ) ) ) : ?>
-
-<?php /*
-		<div class="content-panel__tags">
-
-			<div class="content-panel__tags__inner">
-
-				<?php echo join( ', ', $work_tags ); ?>
-
-			</div>
-			<!-- content-panel tags inner -->
-
-		</div>
-		<!-- content-panel tags -->
-*/ ?>
-
-
-<?php /*
-	<!-- Image -->
-
-	<?php elseif ( '' != get_sub_field( 'image' ) ) : ?>
-
-		<div class="content-panel__image">
-
-			<div class="content-panel__image__content">
-
-				<?php echo rp3_picture_element_v2( esc_attr( get_sub_field( 'image' ) ), 'content' ); ?>
-
-			</div>
-			<!-- content-panel image content -->
-
-		</div>
-		<!-- content-panel image -->
-
-	<?php endif; ?>
-
-</section>
-<!-- content-panel -->
-*/ ?>
