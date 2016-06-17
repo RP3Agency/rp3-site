211067ac9e7cbf2c20749275ad1fae922f0eb216
Tracy Rotton <trotton@rp3agency.com>
2016-06-17 10:34:22 -0400

    [#1255] removing the suggested posts in search, since they were not being kept updated

--- src/theme/template-parts/component-blog-search.php
+++ src/theme/template-parts/component-blog-search.php
@@ -1,19 +1,3 @@
-<?php
-
-/** Pull the (up to) 5 most recent curated search suggestions */
-$args = array(
-	'post_type'			=> 'post',
-	'meta_key'			=> 'search_suggestion',
-	'meta_value'		=> true,
-	'posts_per_page'	=> 5,
-	'orderby'			=> 'date',
-	'order'				=> 'DESC'
-);
-
-$search_suggestions = new WP_Query( $args );
-
-?>
-
 <!-- Full-page Blog Search Takeover -->
 
 <div id="blog-search" class="blog-search">
@@ -25,37 +9,6 @@ $search_suggestions = new WP_Query( $args );
 			<button id="blog-search__close" class="blog-search__close">Close</button>
 
 			<?php get_search_form( true ); ?>
-
-			<aside id="blog-search__suggestions" class="blog-search__suggestions">
-
-				<h2 class="blog-search__header">Featured Opportunities:</h2>
-
-				<?php if ( $search_suggestions->have_posts() ) : ?>
-
-					<ul class="blog-search__list">
-
-						<?php while ( $search_suggestions->have_posts() ) : $search_suggestions->the_post(); ?>
-
-							<li><a href="<?php echo esc_url( get_the_permalink() ); ?>" class="block">
-
-								<div class="blog-search__photo">
-									<?php echo get_the_post_thumbnail( $post->ID, 'four_three_thumb' ); ?>
-								</div>
-
-								<div class="blog-search__details">
-									<span class="link"><?php the_title(); ?></span><br/>
-									<?php echo rp3_byline(); ?>
-								</div>
-
-							</a></li>
-
-						<?php endwhile; ?>
-
-					</ul>
-
-				<?php endif; wp_reset_query(); ?>
-
-			</aside>
 
 			<div id="blog-search__results" class="blog-search__results">
 
