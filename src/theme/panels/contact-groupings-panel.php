53eb491258b6eb76141fb7fb1276ddfd81ccb980
Tracy Rotton <trotton@rp3agency.com>
2016-07-05 12:14:30 -0400

    [#1265] new contact groupings panel

---
+++ src/theme/panels/contact-groupings-panel.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Contact groupings panel.
+ *
+ * @package RP3
+ */
+?>
+
+<!-- Contact Groupings Panel -->
+
+<div class="page__panel__contact-groupings">
+
+	<ul>
+
+		<li>
+			<h2>Work With Us</h2>
+			Jim Lansbury<br/>
+			<a href="mailto:jlansbury@rp3agency.com">jlansbury@rp3agency.com</a>
+		</li>
+
+		<li>
+			Beth Johnson<br/>
+			<a href="mailto:bjohnson@rp3agency.com">bjohnson@rp3agency.com</a>
+		</li>
+
+		<li>
+			Kurt Roberts<br/>
+			<a href="mailto:kroberts@rp3agency.com">kroberts@rp3agency.com</a>
+		</li>
+
+	</ul>
+
+	<ul>
+
+		<li>
+			<h2>Media Inquiries</h2>
+			Chris Finnegan<br/>
+			<a href="mailto:cfinnegan@rp3agency.com">cfinnegan@rp3agency.com</a>
+		</li>
+
+		<li>
+			<h2>General Inquiries</h2>
+			<a href="mailto:info@rp3agency.com">info@rp3agency.com</a>
+		</li>
+
+		<li>
+			<h2>Careeers</h2>
+			<a href="mailto:jobs@rp3agency.com">jobs@rp3agency.com</a>
+		</li>
+
+	</ul>
+
+	<ul>
+
+		<li>
+			<address>
+				7316 Wisconsin Avenue<br/>
+				Suite 450<br/>
+				Bethesda, MD 20814
+			</address>
+		</li>
+
+		<li>
+			t. (301) 718-0333<br/>
+			f. (301) 718-9333
+		</li>
+
+	</ul>
+
+</div>
